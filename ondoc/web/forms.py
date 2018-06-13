357cf495d11f28b88b5efefe2997be883654e1ec
Ronit <ronitd@policybazaar.com>
2018-06-13 15:16:47 +0530

    email validation

--- ondoc/web/forms.py
+++ ondoc/web/forms.py
@@ -14,7 +14,9 @@ class OnlineLeadsForm(ModelForm):
             'mobile': TextInput(attrs={'placeholder': 'Mobile Number', 'class': 'form-control', 'id': 'mobile',
                                        'type': 'number', 'min': 7000000000, 'max': 9999999999, }),
             'email': EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control', 'id': 'email', 'pattern':
-                "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$", 'oninvalid': "setCustomValidity('Enter a Valid Email Address')"}),
+                                        "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$", 'oninvalid':
+                                        "setCustomValidity('Enter a Valid Email Address')",
+                                       'oninput': "setCustomValidity('')"}),
             }
         fields = ['member_type', 'name', 'mobile', 'email']
 
@@ -39,7 +41,9 @@ class CareersForm(ModelForm):
             'mobile': TextInput(attrs={'placeholder': 'Mobile Number', 'class': 'form-control', 'id': 'mobile',
                                        'type': 'number', 'min': 7000000000, 'max': 9999999999, }),
             'email': EmailInput(attrs={'placeholder': 'Email', 'class': 'form-control', 'id': 'email', 'pattern':
-                "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$", 'oninvalid': "setCustomValidity('Enter a Valid Email Address')"}),
+                                        "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$", 'oninvalid':
+                                        "setCustomValidity('Enter a Valid Email Address')",
+                                       'oninput': "setCustomValidity('')"}),
             'resume': FileInput(attrs={'id': 'upload-resume'}),
         }
         fields = ['profile_type', 'name', 'mobile', 'email', 'resume']
