2cea242a39d2db539bd93ddecc6a25f284d388b8
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-06-15 20:29:40 +0530

    changed onboarding otp message

--- ondoc/onboard/views.py
+++ ondoc/onboard/views.py
@@ -50,7 +50,7 @@ def otp(request):
         action = request.POST.get('_resend_otp')
         if action:
             otp = randint(200000, 900000)
-            message = 'You have initiated onboarding process for '+existing.lab.name+'. OTP is '+str(otp)
+            message = 'You have initiated onboarding process on DocPrime for '+existing.lab.name+'. OTP is '+str(otp)
             api.send_sms(message, str(existing.lab.primary_mobile))
 
             # print(otp)
