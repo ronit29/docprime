f151a08b7884e08ed096dc48180f70f488a29488
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-07-16 14:05:46 +0530

    should return exisiting lead instead of creating new

--- ondoc/matrix/views.py
+++ ondoc/matrix/views.py
@@ -28,48 +28,61 @@ class MatrixLead(GenericViewSet):
 
     def create(self, request, *args, **kwargs):
 
-       from .serializers import MatrixLeadDataSerializer
-       response = {"status": "Error"}
-       serializer = MatrixLeadDataSerializer(data=request.data)
-       serializer.is_valid(raise_exception=True)
-       data = serializer.validated_data
-       staff_profile = StaffProfile.objects.filter(employee_id__iexact=data.get('agent_employee_id'))
+        from .serializers import MatrixLeadDataSerializer
+        response = {"status": "error"}
+        serializer = MatrixLeadDataSerializer(data=request.data)
+        is_valid = serializer.is_valid(raise_exception=False)
 
-       if not staff_profile.exists():
-           return Response(status=status.HTTP_400_BAD_REQUEST, data={'status': 'Error', 'message': 'StaffProfile Not Found'})
+        if not is_valid:
+            response['message'] = 'Invalid Request'
+            response['errors'] = serializer.errors
+            return Response(status = status.HTTP_400_BAD_REQUEST, data=response)
 
-       staff_profile = staff_profile.first()
+        data = serializer.validated_data
+        staff_profile = StaffProfile.objects.filter(employee_id__iexact=data.get('agent_employee_id'))
 
-       if data.get('sub_product') == MatrixLead.DOCTOR:
-           doctor_data = {"name": data.get('name'),
-                          'gender': data.get('gender'),
-                          'created_by': staff_profile.user,
-                          'onboarding_status': Doctor.NOT_ONBOARDED,
-                          'assigned_to': staff_profile.user,
-                          'matrix_reference_id': data.get('matrix_reference_id'),
-                          'matrix_lead_id': data.get('matrix_lead_id'),
-           }
-           if data.get('gender') is None:
-               doctor_data.pop('gender', None)
-           create_lead = Doctor.objects.create(**doctor_data)
-           if data.get('phone_number'):
-               DoctorMobile.objects.create(doctor=create_lead, number=data.get('phone_number'), is_primary=False)
-           change_url = "/admin/doctor/doctor/%s/change/"%(create_lead.id)
-       elif data.get('sub_product') == MatrixLead.LAB:
-           create_lead = Lab.objects.create(name=data.get('name'), city=data.get('city'), assigned_to=staff_profile.user,
+        if not staff_profile.exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'status': 'error', 'message': 'StaffProfile Not Found'})
+
+        staff_profile = staff_profile.first()
+
+
+
+        if data.get('sub_product') == MatrixLead.DOCTOR:
+            existing_doctor = Doctor.objects.filter(matrix_lead_id=data.get('matrix_lead_id'))
+            if existing_doctor.exists():
+                create_lead = existing_doctor.first()
+            else:
+                doctor_data = {"name": data.get('name'),
+                              'gender': data.get('gender'),
+                              'created_by': staff_profile.user,
+                              'onboarding_status': Doctor.NOT_ONBOARDED,
+                              'assigned_to': staff_profile.user,
+                              'matrix_reference_id': data.get('matrix_reference_id'),
+                              'matrix_lead_id': data.get('matrix_lead_id'),
+                }
+                if data.get('gender') is None:
+                    doctor_data.pop('gender', None)
+                create_lead = Doctor.objects.create(**doctor_data)
+                if data.get('phone_number'):
+                    DoctorMobile.objects.create(doctor=create_lead, number=data.get('phone_number'), is_primary=False)
+            change_url = "/admin/doctor/doctor/%s/change/"%(create_lead.id)
+
+        elif data.get('sub_product') == MatrixLead.LAB:
+            existing_lab = Lab.objects.filter(matrix_lead_id=data.get('matrix_lead_id'))
+            if existing_lab.exists():
+                create_lead = existing_lab.first()
+
+            else:
+                create_lead = Lab.objects.create(name=data.get('name'), city=data.get('city'), assigned_to=staff_profile.user,
                                              onboarding_status=Lab.NOT_ONBOARDED, created_by=staff_profile.user,
                                             primary_mobile=data.get('phone_number'), matrix_lead_id = data.get('matrix_lead_id'), matrix_reference_id = data.get('matrix_reference_id'))
-           change_url = "/admin/diagnostic/lab/%s/change/"% (create_lead.id)
+            change_url = "/admin/diagnostic/lab/%s/change/"% (create_lead.id)
 
-       if create_lead:
-           response['status'] = 'Success'
-           response['id'] = create_lead.id
-           response['url'] = change_url
+        if create_lead:
+            response['status'] = 'success'
+            response['id'] = create_lead.id
+            response['url'] = change_url
+            response['message'] = 'Lead Created Successfully'
 
-       return Response(response)
-
-
-
-
-
-
+        return Response(response)
