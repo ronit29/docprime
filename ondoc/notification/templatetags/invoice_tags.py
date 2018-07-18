49aef7965c744a74bf074008bec94338844a093f
kanhaiya lal <kanhaiyalal@policybazaar.com>
2018-07-18 15:03:22 +0530

    add custom filter for absolute url

--- ondoc/notification/templatetags/invoice_tags.py
+++ ondoc/notification/templatetags/invoice_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from ondoc.doctor.models import OpdAppointment
 from ondoc.authentication.models import UserProfile
+from urllib.parse import urlparse
+from django.contrib.staticfiles.templatetags.staticfiles import static
+from django.conf import settings
 
 register = template.Library()
 
@@ -22,3 +25,13 @@ def get_gender(gender):
         return ""
     payment_mode_mapping = {choice[0]: choice[1] for choice in UserProfile.GENDER_CHOICES}
     return payment_mode_mapping[gender]
+
+
+@register.filter
+def get_absolute_url(url):
+    url = static(url)
+    if bool(urlparse(url).netloc):
+        return url
+    else:
+        return settings.BASE_URL +url
+
