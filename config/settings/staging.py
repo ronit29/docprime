e7f64ebc07372b0b49ccac677a3b41e2b7d06c8d
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-07-25 19:36:21 +0530

    migrate staging environment to cdn

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -35,3 +35,7 @@ CACHES = {
         'LOCATION': '/var/tmp/django_cache',
     }
 }
+
+CLOUDFRONT_DOMAIN = "qacdn.docprime.com"
+#CLOUDFRONT_ID = "your cloud front id"
+AWS_S3_CUSTOM_DOMAIN = "qacdn.docprime.com" # to make sure the url that the files are served from this domain
