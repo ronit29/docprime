213936a0be08700f029e8ca09805e4a7ebfe3a0b
ronit <ronitd@policybazaar.com>
2018-07-26 17:09:08 +0530

    chat doctor profile

--- ondoc/api/v1/chat/router.py
+++ ondoc/api/v1/chat/router.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ChatSearchedItemsViewSet, DoctorsListViewSet
+from .views import ChatSearchedItemsViewSet, DoctorsListViewSet, DoctorProfileViewSet
 
 urlpatterns = [
     path('chatsearcheditems', ChatSearchedItemsViewSet.as_view({'get': 'list'}), name='searched-items'),
     path('doctors', DoctorsListViewSet.as_view({'get': 'list'}), name='searched-items'),
+    path('doctor/profile/<int:pk>', DoctorProfileViewSet.as_view({'get': 'retrieve'}), name='doctor-profile'),
 ]
