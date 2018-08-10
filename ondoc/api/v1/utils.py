acbf09d2348778a84cf09338231500312f8fe06b
ronit <ronitd@policybazaar.com>
2018-08-10 11:25:36 +0530

    test data api fix

--- ondoc/api/v1/utils.py
+++ ondoc/api/v1/utils.py
@@ -275,27 +275,14 @@ class ErrorCodeMapping(object):
 
 
 def is_valid_testing_data(user, doctor):
-    from ondoc.doctor.models import Doctor
 
-    if user.id is None:
-        return False
-    if (Doctor.objects.filter(id=doctor.id, is_test_doctor=True).exists() and not user.groups.filter(
-            name=constants['TEST_USER_GROUP']).exists()):
-        return False
-    if (Doctor.objects.filter(id=doctor.id, is_test_doctor=False).exists() and user.groups.filter(
-            name=constants['TEST_USER_GROUP']).exists()):
+    if doctor.is_test_doctor and not user.groups.filter(name=constants['TEST_USER_GROUP']).exists():
         return False
     return True
 
-def is_valid_testing_lab_data(user, lab):
-    from ondoc.diagnostic.models import Lab
 
-    if user.id is None:
-        return False
-    if (Lab.objects.filter(id=lab.id, is_test_lab=True).exists() and not user.groups.filter(
-            name=constants['TEST_USER_GROUP']).exists()):
-        return False
-    if (Lab.objects.filter(id=lab.id, is_test_lab=False).exists() and user.groups.filter(
-            name=constants['TEST_USER_GROUP']).exists()):
+def is_valid_testing_lab_data(user, lab):
+
+    if lab.is_test_lab and not user.groups.filter(name=constants['TEST_USER_GROUP']).exists():
         return False
     return True
