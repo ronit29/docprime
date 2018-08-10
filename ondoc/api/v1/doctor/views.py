d6bc8ec87455b37327b28f8ff914eb8e69b9a7ca
shashanksingh <shashanks@policybazaar.com>
2018-08-10 15:17:11 +0530

    uncommented authentication and permission checks

--- ondoc/api/v1/doctor/views.py
+++ ondoc/api/v1/doctor/views.py
@@ -351,8 +351,8 @@ class DoctorAppointmentsViewSet(OndocViewSet):
 
 
 class DoctorProfileView(viewsets.GenericViewSet):
-    # authentication_classes = (TokenAuthentication, )
-    # permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return models.OpdAppointment.objects.all()
@@ -400,7 +400,6 @@ class DoctorProfileView(viewsets.GenericViewSet):
             resp_data["access_type"] = OPD_ONLY
         elif auth_models.GenericLabAdmin.objects.filter(user=user, is_disabled=False).exists():
             resp_data["access_type"] = LAB_ONLY
-
         # Check access_type END
 
         resp_data["count"] = queryset
