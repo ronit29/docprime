b4932d3d7b5e457f00c56097e97fb0733f4ae205
kanhaiya lal <kanhaiyalal@policybazaar.com>
2018-09-06 19:45:49 +0530

    fix minor changes

--- ondoc/api/v1/common/router.py
+++ ondoc/api/v1/common/router.py
@@ -4,7 +4,7 @@ from .views import CitiesViewSet, ServicesViewSet
 urlpatterns = [
     path('cities/list', CitiesViewSet.as_view({'get': 'list'}), name='cities-list'),
     path('generate/pdf', ServicesViewSet.as_view({'post': 'generatepdf'}, ), name='generate-pdf'),
-    path('send/email', ServicesViewSet.as_view({'post': 'send_email'}, ), name='send-email'),
-    path('send/sms', ServicesViewSet.as_view({'post': 'send_sms'}, ), name='send-sms'),
+    # path('send/email', ServicesViewSet.as_view({'post': 'send_email'}, ), name='send-email'),
+    # path('send/sms', ServicesViewSet.as_view({'post': 'send_sms'}, ), name='send-sms'),
     path('chat_prescription/<str:name>', ServicesViewSet.as_view({'get': 'download_pdf'}, ), name='download-pdf'),
 ]
