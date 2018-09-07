49ae95abeab94d519dab93cd129c2286aad0fb60
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-09-07 21:23:53 +0530

    fixes

--- ondoc/tracking/admin.py
+++ ondoc/tracking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Visitor, Visits, VisitorEvents
 # Register your models here.
+admin.site.register(Visitor)
+admin.site.register(Visits)
+admin.site.register(VisitorEvents)
