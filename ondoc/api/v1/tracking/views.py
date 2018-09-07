b08374d6610b4959a0b284d2b90dd2d013ed026f
ronit <ronitd@policybazaar.com>
2018-09-07 14:03:54 +0530

    tracking pi

--- ondoc/api/v1/tracking/views.py
+++ ondoc/api/v1/tracking/views.py
@@ -3,18 +3,29 @@ from rest_framework.response import Response
 from rest_framework import status
 from . import serializers
 from rest_framework.viewsets import GenericViewSet
-
+from rest_framework import status
+import json
 
 class EventCreateViewSet(GenericViewSet):
 
     def create(self, request):
-        serializer = serializers.EventCreateSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        validated_data = serializer.validated_data
-        event_data = validated_data.get('data')
-        if event_data:
-            pass
-        return Response()
+        resp = {}
+        if request.data and isinstance(request.data, dict):
+            event_name = request.data.get('event')
+            if event_name:
+                visit_id = request.session.get('visit_id')
+                if visit_id:
+                    try:
+                        event = track_models.VisitorEvents(name=event_name, data=request.data, visits_id=visit_id)
+                        event.save()
+                    except Exception:
+                        resp['error'] = "Error Processing Event Data!"
+                    resp['success'] = "Response Saved Successfully!"
+            else:
+                resp['error'] = "Event name not Found!"
+        else:
+            return Response({"error": "Invalid Data"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(resp)
 
 
 
