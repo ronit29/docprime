64c85827182660fb7442957e2afb7a83bc4b5f76
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-09-15 20:25:44 +0530

    fixes

--- ondoc/api/v1/common/views.py
+++ ondoc/api/v1/common/views.py
@@ -123,8 +123,11 @@ class SmsServiceViewSet(viewsets.GenericViewSet):
 
 
 class UpdateXlsViewSet():
+    fields = ['longitude', 'latitude', 'radius', 'specialty_id', 'lab_id', 'type', 'test_id']
+    required_headers = fields+['result_count', 'url']
 
     def update(self, request):
+
         search_count_column = None
         serializer = serializers.XlsSerializer(data=request.FILES)
         serializer.is_valid(raise_exception=True)
@@ -132,70 +135,89 @@ class UpdateXlsViewSet():
         file = validated_data.get('file')
         wb = load_workbook(file)
         sheet = wb.active
-        rows = [row for row in sheet.rows]
-        columns = {i+1: column.value.strip().lower() for i, column in enumerate(rows[0]) if column.value}
-        for key in columns.keys():
-            if columns.get(key) == 'number_of_results_on_search_page':
-                search_count_column = key
-        if search_count_column:
-            for i in range(2, len(rows)+1):
-                sheet.cell(row=i, column=search_count_column).value = self.get_result_count(i, columns, sheet)
-            response = HttpResponse(content=save_virtual_workbook(wb),
-                                    content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-            response['Content-Disposition'] = 'attachment; filename=myexport.xlsx'
-            return response
-        return None
 
-    def prepare_query(self, column, value, query):
-        if column == 'test_id' and value is not None and value != '':
-            query.update({
-                'lab_pricing_group__available_lab_tests__test_id__in': [int(value.strip()) for value in
-                                                                        str(value).split(",")],
+        rows = [row for row in sheet.rows]
+        #columns = {i+1: column.value.strip().lower() for i, column in enumerate(rows[0]) if column.value}
+        self.header = {column.value.strip().lower(): i + 1  for i, column in enumerate(rows[0]) if column.value}
+        search_count_column = self.header.get('result_count')
+        url_column = self.header.get('url')
+        validation_url_column = self.header.get('validation_url')
+
+        for i in range(2, len(rows)+1):
+            data = self.get_data(i,sheet)
+            output = self.get_result_count(data)
+            sheet.cell(row=i, column=search_count_column).value = output[0]
+            sheet.cell(row=i, column=url_column).value = output[1]
+            sheet.cell(row=i, column=validation_url_column).value = output[2]
+
+        response = HttpResponse(content=save_virtual_workbook(wb),
+                                    content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=myexport.xlsx'
+        return response
+
+    def get_data(self,row,sheet):
+        data={}
+        for key in self.fields:
+            value = sheet.cell(row=row, column=self.header.get(key)).value
+            if isinstance(value, str):
+                value = value.strip().lower()
+            if key in ['specialty_id','test_id']:
+                value = [int(x) for x in str(value).split(',')] if value else []
+
+            data[key] = value
+        return data
+
+    def get_result_count(self, data):
+        if data['type']=='doctor' :
+            return self.get_doctor_count(data);
+        elif data['type']=='lab' :
+            return self.get_lab_count(data);
+        else :
+            return 0
+
+    def get_doctor_count(self, data):
+        specialty_id = data['specialty_id']
+        latitude = data['latitude']
+        longitude = data['longitude']
+        max_distance = 1000*data['radius']
+
+        results = Doctor.objects.all()
+        results = (Doctor.objects.filter(
+                doctorspecializations__specialization__in=specialty_id
+            ) if len(specialty_id)>0 else Doctor.objects.all())
+
+        search_count = results.filter(
+            hospitals__location__distance_lte=(Point(longitude, latitude), max_distance),
+            is_live=True,
+            is_test_doctor=False,
+            is_internal=False,
+            hospitals__is_live=True).distinct().count()
+        url = "{domain}/opd/searchresults?specializations={specialty_id}&lat={latitude}&long={longitude}&force_location=true".format(domain=settings.CONSUMER_APP_DOMAIN, specialty_id=','.join([str(x) for x in specialty_id]),latitude=str(latitude), longitude=str(longitude))
+        validation_url = url+"&max_distance={max_distance}".format(max_distance=max_distance/1000)
+
+        return (search_count, url, validation_url)
+
+    def get_lab_count(self, data):
+        test_id = data['test_id']
+        #lab_id = data['lab_id']
+        latitude = data['latitude']
+        longitude = data['longitude']
+        max_distance = 1000*data['radius']
+
+        filter = {'location__distance_lte': (Point(longitude, latitude), max_distance)}
+        if len(test_id)>0:
+            filter.update({
+                'lab_pricing_group__available_lab_tests__test_id__in': test_id,
                 'lab_pricing_group__available_lab_tests__enabled': True
             })
-        elif column == 'lab_id' and value is not None and value != '':
-            query.update({
-                'id': int(value)
-            })
-        return query
 
-    def get_result_count(self, row, columns, sheet):
-        longitude, latitude, max_distance, search_count_column, report_type = None, None, None, None, None
-        query = {}
-        for key in columns.keys():
-            cell_value = str(sheet.cell(row=row, column=key).value).strip() if sheet.cell(row=row,
-                                                                                          column=key).value else None
-            if columns.get(key) == 'longitude':
-                longitude = float(cell_value) if cell_value else None
-            elif columns.get(key) == 'latitude':
-                latitude = float(cell_value) if cell_value else None
-            elif columns.get(key) == 'radius_in_km':
-                max_distance = int(cell_value) * 1000 if cell_value else None
-            elif columns.get(key) == 'specialization_ids':
-                specialization_ids = [int(value.strip()) for value in cell_value.split(",")] if cell_value else []
-            elif columns.get(key) == 'type':
-                report_type = cell_value
-            self.prepare_query(columns.get(key), sheet.cell(row=row, column=key).value, query)
+        search_count = Lab.objects.filter(is_test_lab=False, is_live=True,
+                                          lab_pricing_group__isnull=False).filter(**filter).distinct().count()
 
-        if report_type == 'doctor':
-            results = (Doctor.objects.filter(
-                doctorspecializations__specialization__in=specialization_ids
-            ) if specialization_ids else Doctor.objects.all())
-            search_count = results.filter(
-                hospitals__location__distance_lte=(Point(longitude, latitude), max_distance),
-                is_live=True,
-                is_test_doctor=False,
-                is_internal=False,
-                hospitals__is_live=True).distinct().count()
-        else:
-            query.update({
-                'location__distance_lte': (Point(longitude, latitude), max_distance)
-            })
-            search_count = Lab.objects.filter(is_test_lab=False, is_live=True,
-                                              lab_pricing_group__isnull=False).filter(**query).distinct().count()
-        return search_count
+        url = "{domain}/lab/searchresults?test_id={test_id}&lat={latitude}&long={longitude}&force_location=true".format(domain=settings.CONSUMER_APP_DOMAIN, test_id=','.join([str(x) for x in test_id]),latitude=str(latitude), longitude=str(longitude))
+        validation_url = url+"&max_distance={max_distance}".format(max_distance=max_distance/1000)
 
-
+        return (search_count, url, validation_url)
 
 class UpdateXlsViewSet1():
 
