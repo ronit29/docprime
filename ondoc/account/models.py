1b97b79422d0fd2a6a7ac81237616495fa5b88b6
PMirdhaPB <prateekmirdha@policybazaar.com>
2018-09-17 16:12:07 +0530

    Logging pg data on refund

--- ondoc/account/models.py
+++ ondoc/account/models.py
@@ -427,7 +427,8 @@ class ConsumerRefund(TimeStampedModel):
 
         try:
             pg_data = PgTransaction.form_pg_refund_data(consumer_refund_objs)
-            transaction.on_commit(lambda: refund_curl_request(pg_data))
+            logger.error(pg_data)
+            # transaction.on_commit(lambda: refund_curl_request(pg_data))
         except Exception as e:
             logger.error("Error in refund celery - " + str(e))
 
