8c96fd07d970978f9b1ec68d5df6d428af2773a3
Akash Saini <akashs@docprime.com>
2018-09-17 20:55:16 +0530

    modifying the google api

--- ondoc/location/management/commands/map_hospital_locations.py
+++ ondoc/location/management/commands/map_hospital_locations.py
@@ -8,7 +8,7 @@ from django.contrib.gis.db.models.functions import Distance
 
 def map_hospital_locations():
     all_hospitals = Hospital.objects.filter(is_live=True).all().annotate(distance=Distance('location', Point(float(77.0694707),float(28.4502948), srid=4326))).order_by('distance')[:50]
-    print("Attempting for doctors. ", len(all_hospitals))
+    print("Attempting for hospital. ", len(all_hospitals))
     for hospital in all_hospitals:
         success = EntityLocationRelationship.create(latitude=hospital.location.y, longitude=hospital.location.x, content_object=hospital)
         if not success:
--- ondoc/location/service.py
+++ ondoc/location/service.py
@@ -20,50 +20,51 @@ def get_meta_by_latlong(lat, long):
             return []
 
         resp_data = response.json()
-        GeoIpResults(value=json.dumps(resp_data), latitude=lat, longitude=long).save()
+
+        if resp_data.get('status', None) == 'OK' and isinstance(resp_data.get('results'), list) and \
+                len(resp_data.get('results')) > 0:
+            GeoIpResults(value=json.dumps(resp_data), latitude=lat, longitude=long).save()
+        else:
+            logger.info("[ERROR] Google API for fetching the location via latitude and longitude failed.")
+            logger.info("[ERROR] %s", response.reason)
+            return []
 
     else:
         resp_data = json.loads(saved_json.first().value)
 
-    if resp_data.get('status', None) == 'OK' and isinstance(resp_data.get('results'), list) and len(resp_data.get('results')) > 0:
-        result_array = resp_data['results']
+    result_array = resp_data['results']
+    response_list = list()
 
-        response_list = list()
+    # Take the address component with longest length as it can provide us the most relevant address.
+    max_length = 0
+    address_component = None
+    for result_obj in result_array:
+        if len(result_obj.get('address_components', [])) > max_length:
+            address_component = result_obj.get('address_components')
+            max_length = len(result_obj.get('address_components'))
 
-        # Take the address component with longest length as it can provide us the most relevant address.
-        max_length = 0
-        address_component = None
-        for result_obj in result_array:
-            if len(result_obj.get('address_components', [])) > max_length:
-                address_component = result_obj.get('address_components')
-                max_length = len(result_obj.get('address_components'))
-
-        if not address_component:
-            return response_list
-
-        resp_data = dict()
-        # address_component.reverse()
-
-        for component in address_component:
-            for key in component.get('types', []):
-                resp_data[key.upper()] = component['long_name']
-
-        for type in ['COUNTRY', 'ADMINISTRATIVE_AREA_LEVEL_1', 'ADMINISTRATIVE_AREA_LEVEL_2', 'LOCALITY', 'SUBLOCALITY',
-                     'SUBLOCALITY_LEVEL_1', 'SUBLOCALITY_LEVEL_2', 'SUBLOCALITY_LEVEL_3']:
-
-            if type.upper() in resp_data.keys():
-                if type.upper().startswith('SUBLOCALITY_LEVEL'):
-                    response_list.append({'key': 'SUBLOCALITY', 'type': type, 'postal_code': resp_data['POSTAL_CODE'],
-                                          'value': resp_data[type.upper()]})
-                else:
-                    response_list.append({'key': type, 'type': type, 'postal_code': resp_data['POSTAL_CODE'],
-                                          'value': resp_data[type.upper()]})
-
+    if not address_component:
         return response_list
 
-    else:
-        logger.info("[ERROR] Google API for fetching the location via latitude and longitude failed.")
-        logger.info("[ERROR] %s", response.reason)
-        return []
+    resp_data = dict()
+
+    # address_component.reverse()
+    for component in address_component:
+        for key in component.get('types', []):
+            resp_data[key.upper()] = component['long_name']
+
+    for type in ['COUNTRY', 'ADMINISTRATIVE_AREA_LEVEL_1', 'ADMINISTRATIVE_AREA_LEVEL_2', 'LOCALITY', 'SUBLOCALITY',
+                 'SUBLOCALITY_LEVEL_1', 'SUBLOCALITY_LEVEL_2', 'SUBLOCALITY_LEVEL_3']:
+
+        if type.upper() in resp_data.keys():
+
+            if type.upper().startswith('SUBLOCALITY_LEVEL'):
+                response_list.append({'key': 'SUBLOCALITY', 'type': type, 'postal_code': resp_data['POSTAL_CODE'],
+                                      'value': resp_data[type.upper()]})
+            else:
+                response_list.append({'key': type, 'type': type, 'postal_code': resp_data['POSTAL_CODE'],
+                                      'value': resp_data[type.upper()]})
+    return response_list
+
 
 
