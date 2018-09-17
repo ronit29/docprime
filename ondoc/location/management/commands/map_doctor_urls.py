578ea5db8d9953069fa875f1748bb555d9ac3b7f
Akash Saini <akashs@docprime.com>
2018-09-17 20:37:19 +0530

    interim

--- ondoc/location/management/commands/map_doctor_urls.py
+++ ondoc/location/management/commands/map_doctor_urls.py
@@ -4,13 +4,12 @@ from ondoc.location.models import EntityUrls
 
 
 def map_doctor_urls():
-    all_doctors = Doctor.objects.all()
+    all_doctors = Doctor.objects.filter(is_live=True).all()
     for doctor in all_doctors:
-        if doctor.data_status == 3:
-            success = EntityUrls.create(doctor)
-            print("success is", success)
-            if not success:
-                break
+        success = EntityUrls.create(doctor)
+        print("success is", success)
+        if not success:
+            break
 
 
 class Command(BaseCommand):
