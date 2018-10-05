6a975cc2e33d5b0feeb71d9c25934d538dd3c8fa
Sonam Sinha <sonamsinha@policybazaar.com>
2018-10-05 17:15:12 +0530

    interim

--- ondoc/location/management/commands/map_sitemap.py
+++ ondoc/location/management/commands/map_sitemap.py
@@ -3,7 +3,7 @@ from django.core.management.base import BaseCommand
 from django.core.paginator import Paginator
 
 from ondoc.location.models import EntityLocationRelationship, EntityUrls
-from ondoc.seo.models import Sitemap
+from ondoc.seo.models import SitemapManger
 from ondoc.seo.sitemaps import get_sitemap_urls
 from django.template import loader
 
@@ -18,18 +18,17 @@ def map_sitemaps():
 
         for page_num in range(1, paginator.num_pages + 1):
             filename = 'sitemap_%s.xml' % page_num
+            count = len(sitemap_obj.get_urls())
             file = template.render({'urlset': sitemap_obj.get_urls(page_num)})
 
-        name = 'filename_%s' % sitemap_identifier
+            name = 'filename_%s' % sitemap_identifier
 
-        with open(name, 'w') as name:
-            print("Generating sitemap_index.xml %s" % name)
-            # name.write(loader.render_to_string('name.xml', {'sitemaps': sitemap_obj.get_urls()}))
-            name.write(file)
+            with open(name, 'w') as name:
+                print("Generating sitemap_index.xml %s" % name)
+                name.write(file)
 
-
-        print("success for ", sitemap_identifier)
-
+            sitemap_file = SitemapManger(file=name, count=count)
+            sitemap_file.save()
 
 class Command(BaseCommand):
     def handle(self, **options):
