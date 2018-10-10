360a632a4632923fb39a753f8bc7df0f22242a66
navneetdp <navneetsingh@docprime.com>
2018-10-10 18:29:32 +0530

    Api for giving discount of an amount for given user

--- ondoc/api/v1/coupon/views.py
+++ ondoc/api/v1/coupon/views.py
@@ -1,4 +1,7 @@
 from ondoc.coupon import models as coupon_models
+from ondoc.account.models import Order
+from ondoc.doctor.models import OpdAppointment
+from ondoc.diagnostic.models import LabAppointment
 from ondoc.authentication import models as auth_models
 from ondoc.api.v1.utils import CouponsMixin
 from ondoc.api.v1.coupon import serializers as coupon_serializers
@@ -9,6 +12,7 @@ from ondoc.authentication.backends import JWTAuthentication
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from django.contrib.auth import get_user_model
+from rest_framework import status
 from django.db import transaction
 from . import serializers
 from django.conf import settings
@@ -37,3 +41,25 @@ class ApplicableCouponsViewSet(viewsets.GenericViewSet):
                                            "desc": coupon.description})
 
         return Response(applicable_coupons)
+
+
+class CouponDiscountViewSet(viewsets.GenericViewSet):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def coupon_discount(self, request, *args, **kwargs):
+        input_data = request.data
+        coupon_code = input_data.get("coupon_code")
+        deal_price = input_data.get("deal_price")
+        product_id = input_data.get("product_id")
+        if product_id == str(Order.DOCTOR_PRODUCT_ID):
+            obj = OpdAppointment()
+        elif product_id == str(Order.LAB_PRODUCT_ID):
+            obj = LabAppointment()
+        if coupon_code:
+            if not obj.validate_coupon(request.user, coupon_code):
+                return Response({"status":0, "message": "Invalid coupon code for the user"}, status.HTTP_404_NOT_FOUND)
+            else:
+                discount = obj.get_discount(coupon_code, deal_price)
+
+        return Response({"discount": discount, "status": 1})
