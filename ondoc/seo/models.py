6958c6fd2e90844ad36c0471e977ede02ec2bbbe
Akash Saini <akashs@docprime.com>
2018-10-11 20:29:25 +0530

    changes in sitemap

--- ondoc/seo/models.py
+++ ondoc/seo/models.py
@@ -1,17 +1,7 @@
 from django.db import models
 from ondoc.authentication.models import TimeStampedModel
 from django.core.validators import FileExtensionValidator
-from django.core.files.storage import FileSystemStorage
-from django.conf import settings
-import os
 
-
-class OverwriteStorage(FileSystemStorage):
-
-    def get_available_name(self, name, max_length=256):
-        if self.exists(name):
-            os.remove(os.path.join(settings.MEDIA_ROOT, name))
-        return name
 
 # Create your models here.
 class Sitemap(TimeStampedModel):
@@ -29,9 +19,13 @@ class Robot(TimeStampedModel):
 
 
 class SitemapManger(TimeStampedModel):
-    file = models.FileField(upload_to='seo', storage=OverwriteStorage(), validators=[FileExtensionValidator(allowed_extensions=['xml'])])
+    file = models.FileField(upload_to='seo', validators=[FileExtensionValidator(allowed_extensions=['xml'])])
     count = models.PositiveIntegerField(default=0, null=True)
     valid = models.BooleanField(default=True)
+
+    def delete(self, *args, **kwargs):
+        self.file.delete()
+        super(SitemapManger, self).delete(*args, **kwargs)
 
     class Meta:
         db_table = "sitemap_manager"
