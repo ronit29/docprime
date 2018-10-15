ddfb7f458f309e0b92d952a0eb7b71ecc7073ba3
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-10-15 15:48:17 +0530

    remove doctor registration document validation

--- ondoc/crm/admin/doctor.py
+++ ondoc/crm/admin/doctor.py
@@ -330,7 +330,8 @@ class DoctorDocumentFormSet(forms.BaseInlineFormSet):
             if '_submit_for_qc' in self.request.POST or '_qc_approve' in self.request.POST:
                 for key, value in count.items():
                     if key == DoctorDocument.REGISTRATION and value < 1:
-                        raise forms.ValidationError(choices[key] + " is required")
+                        pass
+                        #raise forms.ValidationError(choices[key] + " is required")
 
 
 class HospitalDocumentFormSet(forms.BaseInlineFormSet):
