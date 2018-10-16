98a9eb991ffbdedd8f4d88b8a417dcba1108fb23
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-10-16 23:15:29 +0530

    fixes

--- ondoc/doctor/models.py
+++ ondoc/doctor/models.py
@@ -415,7 +415,6 @@ class Doctor(auth_model.TimeStampedModel, auth_model.QCModel, SearchKey):
             location_models.EntityUrls.create_page_url(self)
 
 
-
     class Meta:
         db_table = "doctor"
 
