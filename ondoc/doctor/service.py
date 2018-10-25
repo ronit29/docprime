0184d3952fe345e6668166b1d1f0584f10cc2ce1
Akash Saini <akashs@docprime.com>
2018-10-25 19:29:46 +0530

    interim commit

--- ondoc/doctor/service.py
+++ ondoc/doctor/service.py
@@ -33,45 +33,61 @@ def get_doctor_detail_from_google(place_sheet_obj):
             candidate = doctor_place_search_data.get('candidates')[0]
             place_id = candidate.get('place_id')
             if not place_id:
+                print('[ERROR] place id not found.')
                 return False
+            else:
+                if not place_sheet_obj.doctor_detail:
+                    # Now hitting the google api for fetching details of the doctor regarding place_id obtained above.
+                    response = requests.get('https://maps.googleapis.com/maps/api/place/details/json',
+                                            params={'key': api_key, 'place_id': place_id})
+                    if response.status_code != status.HTTP_200_OK or not response.ok:
+                        print("[ERROR] Google API for fetching detail on basis of place_id")
+                        print("[ERROR] %s", response.reason)
+                        return False
+                    else:
+                        doctor_detail_search_data = response.json()
+                        place_sheet_obj.doctor_detail = json.dumps(doctor_detail_search_data)
+                        place_sheet_obj.save()
+                else:
+                    doctor_detail_search_data = json.loads(place_sheet_obj.doctor_detail)
+
+                if not doctor_detail_search_data.get('result', None):
+                    print("[ERROR] Invalid data recieved from google detail api for doctor_clinic_address.")
+                    return False
+                else:
+                    result = doctor_detail_search_data.get('result')
+                    # Extract the useful information from the above api.
+                    doctor_formatted_address = result.get('formatted_address', '')
+                    doctor_number = result.get('formatted_phone_number', '')
+                    doctor_international_number = result.get('international_phone_number', '')
+                    doctor_name = result.get('name', '')
+
+                    place_sheet_obj.doctor_name = doctor_name
+                    place_sheet_obj.doctor_international_number = doctor_international_number
+                    place_sheet_obj.doctor_number = doctor_number
+                    place_sheet_obj.doctor_formatted_address = doctor_formatted_address
+                    place_sheet_obj.save()
+
         else:
             print("[ERROR] Invalid data recieved from google api for doctor_clinic_data.")
             return False
 
-        if not place_sheet_obj.doctor_detail:
-            # Now hitting the google api for fetching details of the doctor regarding place_id obtained above.
-            response = requests.get('https://maps.googleapis.com/maps/api/place/details/json',
-                                    params={'key': api_key, 'place_id': place_id})
-            if response.status_code != status.HTTP_200_OK or not response.ok:
-                print("[ERROR] Google API for fetching detail on basis of place_id")
-                print("[ERROR] %s", response.reason)
-                return False
-            else:
-                doctor_detail_search_data = response.json()
-                place_sheet_obj.doctor_detail = json.dumps(doctor_detail_search_data)
-                place_sheet_obj.save()
-        else:
-            doctor_detail_search_data = json.loads(place_sheet_obj.doctor_detail)
-
-        if not doctor_detail_search_data.get('result', None):
-            print("[ERROR] Invalid data recieved from google detail api for doctor_clinic_address.")
-            return False
-
-        result = doctor_detail_search_data.get('result')
-        # Extract the useful information from the above api.
-        doctor_formatted_address = result.get('formatted_address', '')
-        doctor_number = result.get('formatted_phone_number', '')
-        doctor_international_number = result.get('international_phone_number', '')
-        doctor_name = result.get('name', '')
+        return True
+    except Exception as e:
+        print(str(e))
+        return False
 
 
-
+def get_clinic_detail_from_google(place_sheet_obj):
+    api_key = settings.REVERSE_GEOCODING_API_KEY
+    try:
         # For clinic_address.
         if not place_sheet_obj.clinic_place_search:
             # Hitting the google api for find the place for clinic_address.
             request_parameter = place_sheet_obj.clinic_address
-            response = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery',
-                                    params={'key': api_key, 'input': request_parameter})
+            response = requests.get(
+                'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?inputtype=textquery',
+                params={'key': api_key, 'input': request_parameter})
             if response.status_code != status.HTTP_200_OK or not response.ok:
                 print("[ERROR] Google API for fetching clinic_address place id.")
                 print("[ERROR] %s", response.reason)
@@ -83,58 +99,51 @@ def get_doctor_detail_from_google(place_sheet_obj):
         else:
             clinic_place_search_data = json.loads(place_sheet_obj.clinic_place_search)
 
-        if clinic_place_search_data.get('candidates', None) and isinstance(clinic_place_search_data.get('candidates'), list) and len(clinic_place_search_data.get('candidates')) > 0:
+        if clinic_place_search_data.get('candidates', None) and isinstance(clinic_place_search_data.get('candidates'),list)\
+                and len(clinic_place_search_data.get('candidates')) > 0:
             candidate = clinic_place_search_data.get('candidates')[0]
             place_id = candidate.get('place_id')
             if not place_id:
-                return False
-        else:
-            print("[ERROR] Invalid data recieved from google api for clinic_address.")
-            return False
-
-        if not place_sheet_obj.clinic_detail:
-            # Now hitting the google api for fetching details of the doctor regarding place_id obtained above.
-            response = requests.get('https://maps.googleapis.com/maps/api/place/details/json',
-                                    params={'key': api_key, 'place_id': place_id})
-            if response.status_code != status.HTTP_200_OK or not response.ok:
-                print("[ERROR] Google API for fetching detail on basis of place_id for clinic_address")
-                print("[ERROR] %s", response.reason)
+                print('[ERROR] place id not found.')
                 return False
             else:
-                clinic_detail_search_data = response.json()
-                place_sheet_obj.clinic_detail = json.dumps(clinic_detail_search_data)
-                place_sheet_obj.save()
+                if not place_sheet_obj.clinic_detail:
+                    # Now hitting the google api for fetching details of the clinic regarding place_id obtained above.
+                    response = requests.get('https://maps.googleapis.com/maps/api/place/details/json',
+                                            params={'key': api_key, 'place_id': place_id})
+                    if response.status_code != status.HTTP_200_OK or not response.ok:
+                        print("[ERROR] Google API for fetching detail on basis of place_id")
+                        print("[ERROR] %s", response.reason)
+                        return False
+                    else:
+                        clinic_detail_search_data = response.json()
+                        place_sheet_obj.clinic_detail = json.dumps(clinic_detail_search_data)
+                        place_sheet_obj.save()
+                else:
+                    clinic_detail_search_data = json.loads(place_sheet_obj.clinic_detail)
+
+                if not clinic_detail_search_data.get('result', None):
+                    print("[ERROR] Invalid data recieved from google detail api for clinic_address.")
+                    return False
+                else:
+                    result = clinic_detail_search_data.get('result')
+                    # Extract the useful information from the above api.
+                    clinic_formatted_address = result.get('formatted_address', '')
+                    clinic_number = result.get('formatted_phone_number', '')
+                    clinic_international_number = result.get('international_phone_number', '')
+                    clinic_name = result.get('name', '')
+
+                    place_sheet_obj.clinic_name = clinic_name
+                    place_sheet_obj.clinic_international_number = clinic_international_number
+                    place_sheet_obj.clinic_number = clinic_number
+                    place_sheet_obj.clinic_formatted_address = clinic_formatted_address
+                    place_sheet_obj.save()
+
         else:
-            clinic_detail_search_data = json.loads(place_sheet_obj.clinic_detail)
-
-        if not clinic_detail_search_data.get('result', None):
-            print("[ERROR] Invalid data recieved from google detail api for clinic_address.")
+            print("[ERROR] Invalid data recieved from google api for clinic_data.")
             return False
-
-        result = clinic_detail_search_data.get('result')
-        # Extract the useful information from the above api.
-        clinic_formatted_address = result.get('formatted_address', '')
-        clinic_number = result.get('formatted_phone_number', '')
-        clinic_international_number = result.get('international_phone_number', '')
-        clinic_name = result.get('name', '')
-
-        place_sheet_obj.doctor_place_search = json.dumps(doctor_place_search_data)
-        place_sheet_obj.clinic_place_search = json.dumps(clinic_place_search_data)
-        place_sheet_obj.doctor_detail = json.dumps(doctor_detail_search_data)
-        place_sheet_obj.clinic_detail = json.dumps(clinic_detail_search_data)
-        place_sheet_obj.doctor_number = doctor_number
-        place_sheet_obj.clinic_number = clinic_number
-        place_sheet_obj.doctor_international_number = doctor_international_number
-        place_sheet_obj.clinic_international_number = clinic_international_number
-        place_sheet_obj.doctor_formatted_address = doctor_formatted_address
-        place_sheet_obj.clinic_formatted_address = clinic_formatted_address
-        place_sheet_obj.doctor_name = doctor_name
-        place_sheet_obj.clinic_name = clinic_name
-        place_sheet_obj.save()
 
         return True
     except Exception as e:
         print(str(e))
         return False
-
-
