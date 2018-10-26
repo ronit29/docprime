0fad3fb434ba7f1260b736848ebc003be6772e45
shashanksingh <shashanks@policybazaar.com>
2018-10-26 12:52:02 +0530

    apis created

--- ondoc/api/v1/procedure/serializers.py
+++ ondoc/api/v1/procedure/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from ondoc.procedure.models import Procedure
+from ondoc.procedure.models import Procedure, DoctorClinicProcedure
 
 
-class ProcedureListSerializer(serializers.ModelSerializer):
+class ProcedureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Procedure
         fields = ('id', 'name', 'details')
 
+
+class DoctorClinicProcedureSerializer(serializers.ModelSerializer):
+    procedure = ProcedureSerializer(read_only=True)
+
+    class Meta:
+        model = DoctorClinicProcedure
+        fields = ('procedure', 'mrp', 'agreed_price', 'listing_price')
+
+
+class DoctorClinicProcedureDetailSerializer(serializers.Serializer):
+    doctor_clinic = serializers.IntegerField()
+    procedure = serializers.IntegerField(required=False)
--- ondoc/api/v1/procedure/views.py
+++ ondoc/api/v1/procedure/views.py
@@ -1,17 +1,23 @@
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 import re
 from django.db.models.functions import StrIndex
 from django.db.models import Value, Q
 from ondoc.api.pagination import paginate_queryset
-from ondoc.procedure.models import Procedure
+from ondoc.api.v1.procedure.serializers import DoctorClinicProcedureSerializer, DoctorClinicProcedureDetailSerializer
+from ondoc.procedure.models import Procedure, DoctorClinicProcedure
 from rest_framework.response import Response
 from ondoc.api.v1.procedure import serializers as procedure_serializer
 
 
 class ProcedureListViewSet(GenericViewSet):
 
+    def get_queryset(self):
+        return Procedure.objects.all().prefetch_related('categories')
+
     def list(self, request):
-        name = request.query_params.get('name')
+        name = request.query_params.get('name', None)
+        category = request.query_params.get('category', None)
+
         procedure_data = dict()
         if name:
             search_key = re.findall(r'[a-z0-9A-Z.]+', name)
@@ -23,9 +29,31 @@ class ProcedureListViewSet(GenericViewSet):
                 Q(search_key__istartswith=search_key)).annotate(search_index=StrIndex('search_key', Value(search_key))
                                                                 ).order_by('search_index')
             procedure_queryset = paginate_queryset(procedure_queryset, request)
+        elif category:
+            search_key = re.findall(r'[a-z0-9A-Z.]+', category)
+            search_key = " ".join(search_key).lower()
+            search_key = "".join(search_key.split("."))
+            procedure_queryset = Procedure.objects.filter(
+                Q(categories__search_key__icontains=search_key) |
+                Q(categories__search_key__icontains=' ' + search_key) |
+                Q(categories__search_key__istartswith=search_key)).annotate(search_index=StrIndex('search_key', Value(search_key))
+                                                                ).order_by('search_index').distinct()
+            procedure_queryset = paginate_queryset(procedure_queryset, request)
         else:
-            procedure_queryset = self.queryset[:20]
+            procedure_queryset = self.get_queryset()[:20]
 
-        procedure_list_serializer = procedure_serializer.ProcedureListSerializer(procedure_queryset, many=True)
+        procedure_list_serializer = procedure_serializer.ProcedureSerializer(procedure_queryset, many=True)
         procedure_data['procedures'] = procedure_list_serializer.data
         return Response(procedure_data)
+
+
+class DoctorClinicProcedureViewSet(ModelViewSet):
+    serializer_class = DoctorClinicProcedureSerializer
+    queryset = DoctorClinicProcedure.objects.all()
+
+    def details(self, request):
+        serializer = DoctorClinicProcedureDetailSerializer(data=request.GET)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        result = DoctorClinicProcedureSerializer(self.get_queryset().filter(**validated_data), many=True)
+        return Response(result.data)
