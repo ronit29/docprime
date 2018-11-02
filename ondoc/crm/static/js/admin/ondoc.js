d85406438953fac65de11571e6c187e02c9e1d7b
shashanksingh <shashanks@policybazaar.com>
2018-11-02 12:22:12 +0530

    fix

--- ondoc/crm/static/js/admin/ondoc.js
+++ ondoc/crm/static/js/admin/ondoc.js
@@ -1,41 +1,39 @@
- var $ = django.jQuery;
-
-$( document ).ready(function(){
-    if ($("div.field-cancel_type") && $("div.field-cancellation_reason") && $("div.field-cancellation_comments")){
-        $("div.field-cancel_type").hide();
-        $("div.field-cancellation_reason").hide();
-        $("div.field-cancellation_comments").hide();
-        var status = $("div.field-status select option:selected").val();
+jQuery(document).ready(function(){
+    if (jQuery("div.field-cancel_type") && jQuery("div.field-cancellation_reason") && jQuery("div.field-cancellation_comments")){
+        jQuery("div.field-cancel_type").hide();
+        jQuery("div.field-cancellation_reason").hide();
+        jQuery("div.field-cancellation_comments").hide();
+        var status = jQuery("div.field-status select option:selected").val();
         if(status==6){
-                $("div.field-cancel_type").show();
-                $("div.field-cancellation_reason").show();
-                $("div.field-cancellation_comments").show();
+                jQuery("div.field-cancel_type").show();
+                jQuery("div.field-cancellation_reason").show();
+                jQuery("div.field-cancellation_comments").show();
         }
         else{
-                $("div.field-cancel_type").hide();
-                $("div.field-cancellation_reason").hide();
-                $("div.field-cancellation_comments").hide();
+                jQuery("div.field-cancel_type").hide();
+                jQuery("div.field-cancellation_reason").hide();
+                jQuery("div.field-cancellation_comments").hide();
         }
-        $('div.field-status select').change(function(){
-            var status = $("div.field-status select option:selected").val();
+        jQuery('div.field-status select').change(function(){
+            var status = jQuery("div.field-status select option:selected").val();
             if(status==6){
-                    $("div.field-cancel_type").show();
-                    $("div.field-cancellation_reason").show();
-                    $("div.field-cancellation_comments").show();
+                    jQuery("div.field-cancel_type").show();
+                    jQuery("div.field-cancellation_reason").show();
+                    jQuery("div.field-cancellation_comments").show();
             }
             else{
-                    $("div.field-cancel_type").hide();
-                    $("div.field-cancellation_reason").hide();
-                    $("div.field-cancellation_comments").hide();
+                    jQuery("div.field-cancel_type").hide();
+                    jQuery("div.field-cancellation_reason").hide();
+                    jQuery("div.field-cancellation_comments").hide();
             }
         });
     }
-	$('body').on('change','#doctormobile_set-group .field-is_primary input', function() {
-	    $(this).closest('#doctormobile_set-group').find('.field-is_primary input').not(this).prop('checked',false)
+	jQuery('body').on('change','#doctormobile_set-group .field-is_primary input', function() {
+	    jQuery(this).closest('#doctormobile_set-group').find('.field-is_primary input').not(this).prop('checked',false)
 	 });
 
-	$('body').on('change','#doctoremail_set-group .field-is_primary input', function() {
-	    $(this).closest('#doctoremail_set-group').find('.field-is_primary input').not(this).prop('checked',false)
+	jQuery('body').on('change','#doctoremail_set-group .field-is_primary input', function() {
+	    jQuery(this).closest('#doctoremail_set-group').find('.field-is_primary input').not(this).prop('checked',false)
 	 });
 
 
