fd5867c31464f85f7731c89cc759c1c563800efe
navneetdp <navneetsingh@docprime.com>
2018-11-02 16:22:28 +0530

    migration fix

--- ondoc/coupon/migrations/0004_auto_20181029_1226.py
+++ ondoc/coupon/migrations/0004_auto_20181029_1226.py
@@ -46,8 +46,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='coupon',
             name='test',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
-                                    to='diagnostic.LabTest'),
+            field=models.ManyToManyField(blank=True, null=True, to='diagnostic.LabTest'),
         ),
         migrations.AddField(
             model_name='coupon',
