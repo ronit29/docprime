c7a850d1e9b92dece5278d71e1ab9244f0616bbd
Akash Saini <akashs@docprime.com>
2018-11-16 20:35:09 +0530

    api sync to mongo

--- ondoc/elastic/tasks.py
+++ ondoc/elastic/tasks.py
@@ -16,9 +16,16 @@ import os
 from django.db import connection, transaction
 import psycopg2
 from decimal import Decimal
+from django.conf import settings
+from pymongo import MongoClient
 
 @task(bind=True, max_retries=2)
 def fetch_and_upload_json(self, data):
+
+    mongo_client = MongoClient(settings.MONGO_URL)
+    db = mongo_client.elastic
+    collection = db.doctordata
+
     try:
         obj_id = data.get('id', None)
         obj = elastic_models.DemoElastic.objects.filter(id=obj_id).first()
@@ -32,15 +39,15 @@ def fetch_and_upload_json(self, data):
                     return str(obj)
                 raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
 
-            new_file_name = str(slugify('%s' % str(obj.created_at)))
-            new_file_name = 'demoelastic/%s.json' % new_file_name
+            # new_file_name = str(slugify('%s' % str(obj.created_at)))
+            # new_file_name = 'demoelastic/%s.json' % new_file_name
 
             # f = default_storage.open(new_file_name, 'wb')
             # f.write('['.encode())
             # f.close()
 
-            file = TemporaryUploadedFile(new_file_name, 'byte', 66666, 'utf-8')
-            file.write('['.encode())
+            # file = TemporaryUploadedFile(new_file_name, 'byte', 66666, 'utf-8')
+            # file.write('['.encode())
             batch_size = 50000
             with transaction.atomic():
 
@@ -51,40 +58,50 @@ def fetch_and_upload_json(self, data):
                     response_list = list()
                     for row in cursor:
                         response_list.append(row)
-                        if len(response_list)>=batch_size:
+                        if len(response_list) >= batch_size:
+                            response_list = json.loads(json.dumps(response_list, default=default))
+                            result = collection.insert_many(response_list, ordered=False)
+                            print('Successfully uploaded the chunk of ', len(result.inserted_ids))
                             # file = default_storage.open(new_file_name, 'ab')
-                            content = json.dumps(response_list, default=default).encode()
-                            if not counter == 0:
-                                file.write(','.encode())
+                            # content = json.dumps(response_list, default=default).encode()
+                            # if not counter == 0:
+                                # file.write(','.encode())
 
-                            file.write(content[1:len(content)-1])
+                            # file.write(content[1:len(content)-1])
                             # file.close()
 
                             response_list = list()
+                            result = list()
                             counter+=1
                             #print(str(counter))
 
                     # write all remaining records
-                    if len(response_list)>0:
-                            content = json.dumps(response_list, default=default).encode()
-                            # file = default_storage.open(new_file_name, 'ab')
-                            if not counter == 0:
-                                file.write(','.encode())
+                    if len(response_list) > 0:
+                        response_list = json.loads(json.dumps(response_list, default=default))
+                        result = collection.insert_many(response_list, ordered=False)
+                        print('Successfully uploaded the chunk of ', len(result.inserted_ids))
+                            # content = json.dumps(response_list, default=default).encode()
 
-                            file.write(content[1:len(content)-1])
+                            # file = default_storage.open(new_file_name, 'ab')
+                            # if not counter == 0:
+                                # file.write(','.encode())
+
+                            # file.write(content[1:len(content)-1])
                             # file.close()
 
 
             # file = default_storage.open(new_file_name, 'ab')
-            file.write(']'.encode())
+            # file.write(']'.encode())
 
-            file.seek(0)
-            file.flush()
-            obj.file = file
-            obj.save()
-            print(file.temporary_file_path())
-
-            file.close()
+            # file.seek(0)
+            # file.flush()
+            # obj.file = file
+            # obj.save()
+            # print(file.temporary_file_path())
+            #
+            # file.close()
+            print("Sync to mongo successfull.")
 
     except Exception as e:
+        print("Error in Celery. Failed to upload to mongo.")
         logger.error("Error in Celery. Failed creating json and uploading S3 - " + str(e))
