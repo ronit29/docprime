6ccb0ba9a2c19bb94194f5ed70f6a9b2bcb488c6
ronit <ronitd@policybazaar.com>
2018-11-19 14:15:43 +0530

    count optimize

--- ondoc/api/v1/ratings/serializers.py
+++ ondoc/api/v1/ratings/serializers.py
@@ -81,7 +81,7 @@ class RatingsGraphSerializer(serializers.Serializer):
         return response
 
     def get_rating_count(self, obj):
-        count = obj.count()
+        count = len(obj) if len(obj) > 0 else 0
         return count
 
     def get_review_count(self, obj):
@@ -94,7 +94,7 @@ class RatingsGraphSerializer(serializers.Serializer):
                  3: {'count': 0, 'percent': 0},
                  4: {'count': 0, 'percent': 0},
                  5: {'count': 0, 'percent': 0}}
-        total = obj.count()
+        total = len(obj) if len(obj) > 0 else 0
         if total:
             for rate in obj.all():
                 star_data[rate.ratings]['count'] += 1
