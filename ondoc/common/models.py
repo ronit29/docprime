57cde208e3cb6752e97667acfc483d36b4dc1945
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-11-30 23:29:02 +0530

    use headless chrome for rendering pdf

--- ondoc/common/models.py
+++ ondoc/common/models.py
@@ -2,8 +2,15 @@ from django.db import models
 from weasyprint import HTML, CSS
 import string
 import random
-from django.core.files.uploadedfile import SimpleUploadedFile
+from django.core.files.uploadedfile import SimpleUploadedFile, InMemoryUploadedFile
 from weasyprint.fonts import FontConfiguration
+from hardcopy import bytestring_to_pdf
+import io
+#from tempfile import NamedTemporaryFile
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+from django.core.files import File
+from io import BytesIO
 
 class Cities(models.Model):
     name = models.CharField(max_length=48, db_index=True)
@@ -32,14 +39,29 @@ class PDFTester(models.Model):
     file = models.FileField(upload_to='common/pdf/test', blank=True)
 
     def save(self, *args, **kwargs):
-        css = CSS(string=self.css)
-        font_config = FontConfiguration()
-        pdf_file = HTML(string=self.html).write_pdf(stylesheets=[css])
+
         random_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(12)])
-        name = random_string + '.pdf'
-        file = SimpleUploadedFile(name, pdf_file, content_type='application/pdf')
-        self.file = file
+        path = 'common/pdf/test/' + random_string + '.pdf'
+        file = default_storage.open(path, 'wb')
+
+        extra_args = {
+            'virtual-time-budget': 6000
+        }
+
+        bytestring_to_pdf(self.html.encode(), file, **extra_args)
+
+        file.close()
+
+        ff = File(default_storage.open(path, 'rb'))
+
+        random_string = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(12)])
+        name = random_string+'.pdf'
+
+        self.file = InMemoryUploadedFile(ff.file, None, 'aaa.pdf', 'application/pdf', ff.file.tell(), None)
+
+
         super().save(*args, **kwargs)
+
 
     class Meta:
         db_table = 'pdftest'
