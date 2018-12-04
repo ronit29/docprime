ebc31bb8eceeaac166923cb5796c39d9e6c0914b
Ankush Gupta <ankushg@docprime.com>
2018-12-04 17:55:36 +0530

    profile blank allowed

--- ondoc/api/v1/insurance/serializers.py
+++ ondoc/api/v1/insurance/serializers.py
@@ -58,7 +58,7 @@ class MemberListSerializer(serializers.Serializer):
     address = serializers.CharField(max_length=250)
     pincode = serializers.IntegerField()
     member_type = serializers.ChoiceField(choices=InsuredMembers.MEMBER_TYPE_CHOICES)
-    profile = serializers.PrimaryKeyRelatedField(queryset=UserProfile.objects.all(), allow_null=True)
+    profile = serializers.PrimaryKeyRelatedField(queryset=UserProfile.objects.all(), allow_null=True, blank=True)
     gender = serializers.ChoiceField(choices=InsuredMembers.GENDER_CHOICES)
     town = serializers.CharField(max_length=100)
     district = serializers.CharField(max_length=100)
