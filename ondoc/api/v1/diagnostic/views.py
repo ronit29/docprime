cdb68db9366f6140512a85a6e266c356e96f5767
shashanksingh <shashanks@policybazaar.com>
2018-12-07 14:45:39 +0530

    other category in lab test category

--- ondoc/api/v1/diagnostic/views.py
+++ ondoc/api/v1/diagnostic/views.py
@@ -1136,7 +1136,8 @@ class LabTestCategoryListViewSet(viewsets.GenericViewSet):
     # queryset = None
     def get_queryset(self):
         return None
-    def list(self,request):
+
+    def list(self, request):
         parameters = request.query_params
         try:
             lab_tests = parameters.get('lab_tests', None)
@@ -1146,25 +1147,48 @@ class LabTestCategoryListViewSet(viewsets.GenericViewSet):
         except:
             return Response([], status= status.HTTP_400_BAD_REQUEST)
         if lab_tests:
-            categories = LabTestCategory.objects.prefetch_related('lab_tests').filter(lab_tests__id__in=lab_tests, is_live = True)
+            categories = LabTestCategory.objects.prefetch_related('lab_tests').filter(lab_tests__is_package=False,
+                                                                                      lab_tests__id__in=lab_tests,
+                                                                                      is_live=True, is_package_category=False)
         else:
-            categories = LabTestCategory.objects.prefetch_related('lab_tests').filter(is_live= True)
+            categories = LabTestCategory.objects.prefetch_related('lab_tests').filter(lab_tests__is_package=False,
+                                                                                      is_live=True,
+                                                                                      is_package_category=False)
         empty = []
-        if not categories:
-            return Response([])
+        not_in_others = set()
         for lab_test_category in categories:
-            resp = {}
+            resp = dict()
             resp['category_name'] = lab_test_category.name
             resp['category_id'] = lab_test_category.id
             temp_tests = []
             for lab_test in lab_test_category.lab_tests.all():
-                name = lab_test.name
-                id = lab_test.id
-                if lab_tests and id in lab_tests:
-                    is_selected = True
-                else:
-                    is_selected = False
-                temp_tests.append({'name': name, 'id': id, 'is_selected': is_selected})
+                if not lab_test.is_package:
+                    name = lab_test.name
+                    id = lab_test.id
+                    if lab_tests and id in lab_tests:
+                        is_selected = True
+                        not_in_others.add(id)
+                    else:
+                        is_selected = False
+                    if not is_selected:
+                        temp_tests.append({'name': name, 'id': id, 'is_selected': is_selected})
+                    else:
+                        temp_tests.insert(0, {'name': name, 'id': id, 'is_selected': is_selected})
             resp['tests'] = temp_tests
             empty.append(resp)
+
+        if lab_tests:
+            others = lab_tests.difference(not_in_others)
+            if others:
+                resp = dict()
+                resp['category_name'] = 'Others'
+                resp['category_id'] = -1
+                temp_tests = []
+                for lab_test in LabTest.objects.filter(id__in=others, is_package=False):
+                    name = lab_test.name
+                    id = lab_test.id
+                    is_selected = True
+                    temp_tests.append({'name': name, 'id': id, 'is_selected': is_selected})
+                resp['tests'] = temp_tests
+                empty.append(resp)
         return Response(empty)
