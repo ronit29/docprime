619c97430dddbeb9c1f2266d60a8f85f56a817d3
shreyas <shreyassridhar@docprime.com>
2018-12-13 17:31:10 +0530

    banners

--- ondoc/api/v1/banner/views.py
+++ ondoc/api/v1/banner/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from ondoc.banner.models import Banner
 
@@ -6,5 +7,17 @@ from ondoc.banner.models import Banner
 class BannerListViewSet(viewsets.GenericViewSet):
 
     def list(self, request):
-        queryset = Banner.objects.all()
+        parameters = request.query_params
+
+        try:
+
+            choice_id = parameters.get('choice_id', None)
+            if choice_id:
+                choice_id = set(choice_id)
+
+        except:
+            return Response([], status=status.HTTP_400_BAD_REQUEST)
+
+
+        queryset = Banner.objects.filter(slider_location__in=choice_id)
 
