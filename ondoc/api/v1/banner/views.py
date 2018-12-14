80cbee357c5f9aab878d2e2d2132ed302719734b
shreyas <shreyassridhar@docprime.com>
2018-12-14 18:44:58 +0530

    interim commit

--- ondoc/api/v1/banner/views.py
+++ ondoc/api/v1/banner/views.py
@@ -19,14 +19,16 @@ class BannerListViewSet(viewsets.GenericViewSet):
             return Response([], status=status.HTTP_400_BAD_REQUEST)
 
 
-        queryset = Banner.objects.filter(slider_location__in=choice_id, enable = True)
+        queryset = Banner.objects.filter(slider_locate__in=choice_id, enable = True)
         resp = dict()
+        slider_action = dict(Banner.slider_choice)
+        slider_locate = dict(Banner.slider_location)
         final_result = []
         for data in queryset:
             resp['title'] = data.title
             resp['id'] = data.id
-            resp['slider_location'] = data.slider_location
-            resp['slider_action'] = data.slider_action
+            resp['slider_location'] = slider_locate[data.slider_locate]
+            resp['slider_action'] = slider_action[data.slider_action]
             resp['start_date'] = data.start_date
             resp['end_date'] = data.end_date
             resp['event_name'] = data.event_name
--- ondoc/banner/models.py
+++ ondoc/banner/models.py
@@ -7,21 +7,22 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class Banner(auth_model.TimeStampedModel):
-    LAB_TEST = 1
-    PROCEDURE = 2
+    TEST = 1
+    PROCEDURES = 2
     SPECIALIZATION = 3
     PROCEDURE_CATEGORY = 4
-    slider_choice = [(LAB_TEST, 'Lab Test'), (PROCEDURE, 'Procedure'), (PROCEDURE_CATEGORY, 'Procedure Category'), (SPECIALIZATION, 'Specialization')]
+    CONDITION = 5
+    slider_choice = [(TEST, 'test'), (PROCEDURES, 'procedure'), (PROCEDURE_CATEGORY, 'procedure_category'), (SPECIALIZATION, 'specialization'), (CONDITION, 'condition')]
     HOME_PAGE = 1
     DOCTOR_RESULT = 2
     LAB_RESULT = 3
 
-    slider_location = [(HOME_PAGE, 'Home Page'), (DOCTOR_RESULT, 'Doctor Search Page'), (LAB_RESULT, 'Lab Search Page')]
+    slider_location = [(HOME_PAGE, 'home_page'), (DOCTOR_RESULT, 'doctor_search_page'), (LAB_RESULT, 'lab_search_page')]
     title = models.CharField(max_length=500)
     image = models.ImageField('Banner image', upload_to='banner/images')
     url = models.URLField(blank=True)
     # slider_id = models.PositiveSmallIntegerField(blank=True, null=True, unique=True)
-    slider_location = models.SmallIntegerField(choices=slider_location)
+    slider_locate = models.SmallIntegerField(choices=slider_location)
     slider_action = models.SmallIntegerField(choices=slider_choice)
     object_id = models.PositiveIntegerField()
     start_date = models.DateTimeField()
@@ -36,6 +37,5 @@ class Banner(auth_model.TimeStampedModel):
 
     class Meta:
         db_table = 'banner'
-
 
 
