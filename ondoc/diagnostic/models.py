e3f277c6b5d28cb0c09ef405fbab729468ea1ccd
Arun Chaudhary <arunchaudhary@policybazaar.com>
2018-12-14 15:54:22 +0530

    fixes

--- ondoc/diagnostic/models.py
+++ ondoc/diagnostic/models.py
@@ -998,7 +998,7 @@ class LabAppointment(TimeStampedModel, CouponsMixin):
                 out_obj = self.outstanding_create()
                 self.outstanding = out_obj
         except Exception as e:
-            logger.error("Error while creating outstanding for lab- " + str(self.id))
+            logger.error("Error while creating outstanding for lab- " + str(e))
 
         try:
             # while completing appointment, add a merchant_payout entry
@@ -1006,7 +1006,7 @@ class LabAppointment(TimeStampedModel, CouponsMixin):
                 if self.merchant_payout is None:
                     self.save_merchant_payout()
         except Exception as e:
-            logger.error("Error while saving payout mercahnt for lab- " + str(self.id))
+            logger.error("Error while saving payout mercahnt for lab- " + str(e))
 
         push_to_matrix = kwargs.get('push_again_to_matrix', True)
         if 'push_again_to_matrix' in kwargs.keys():
