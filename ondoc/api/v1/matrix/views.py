7f95d1a409caaba27f53afb181cdc7265973676f
Akash Saini <akashs@docprime.com>
2018-12-17 18:57:51 +0530

    interim  commit

--- ondoc/api/v1/matrix/views.py
+++ ondoc/api/v1/matrix/views.py
@@ -7,8 +7,9 @@ logger = logging.getLogger(__name__)
 from .serializers import NumberMaskSerializer
 from ondoc.authentication import models as auth_models
 from ondoc.doctor import models as doctor_model
-from ondoc.matrix.tasks import get_masked_number
 from django.utils import timezone
+import requests
+import json
 
 
 class MaskNumberViewSet(viewsets.GenericViewSet):
@@ -44,14 +45,30 @@ class MaskNumberViewSet(viewsets.GenericViewSet):
             final = '0'+str(doctor_details.get('std_code')).lstrip('0')+str(doctor_details.get('number')).lstrip('0')
 
         request_data = {
-            "ExpiryDate": int(timezone.now().timestamp()),
-            "FromNumber": data.get('mobile'),
-            "ToNumber": final
+            "ExpirationDate": int((timezone.now() + timezone.timedelta(days=2)).timestamp()),
+            "FromNumber": data.get('mobile') if str(data.get('mobile')).startswith('0') else "0%d" % data.get('mobile'),
+            "ToNumber": final if final.startswith('0') else "0%s" % final
         }
 
-        request_response_data = get_masked_number(request_data)
+        request_response_data = self.get_masked_number(request_data)
         if not request_response_data:
             return Response({'status': 0, 'message': 'No Contact Number found'}, status.HTTP_404_NOT_FOUND)
 
         return Response({'status': 1, 'number': request_response_data}, status.HTTP_200_OK)
 
+    def get_masked_number(self, data):
+        try:
+            url = "http://qamatrixapi.docprime.com/communication/Communication.svc/GetVirtualNumber"
+            response = requests.post(url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+
+            if response.status_code != status.HTTP_200_OK or not response.ok:
+                logger.info("[ERROR] Could not mask the number from matrix system")
+                logger.error("[ERROR] %s", response.reason)
+                return None
+            else:
+                resp_data = response.json()
+                return resp_data
+
+        except Exception as e:
+            logger.error("[ERROR] Could not mask the number from matrix system ", str(e))
+            return None
