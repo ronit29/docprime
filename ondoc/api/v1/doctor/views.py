56e90716dd3137e689bb9162470f0b3a9db4f5ed
ronit <ronitd@policybazaar.com>
2019-01-22 20:14:44 +0530

    fix

--- ondoc/api/v1/doctor/views.py
+++ ondoc/api/v1/doctor/views.py
@@ -874,8 +874,8 @@ class PrescriptionFileViewset(OndocViewSet):
         prescription_file_serializer.is_valid(raise_exception=True)
         prescription_file_serializer.save()
         # resp_data = prescription_file_serializer.data
-        resp_data = serializers.DoctorAppointmentRetrieveSerializer(validated_data.get('appointment'), many=True,
-                                                                         context={'request': request})
+        resp_data = serializers.DoctorAppointmentRetrieveSerializer(validated_data.get('appointment'),
+                                                                         context={'request': request}).data
 
         return Response(resp_data)
 
