126261f5d6d11fef403103162cc990bbb0e93573
Akash Saini <akashs@docprime.com>
2019-01-23 18:02:50 +0530

    initial commit integrations

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,7 +136,8 @@ LOCAL_APPS = (
     'ondoc.procedure',
     'ondoc.elastic',
     'ondoc.banner',
-    'ondoc.ckedit'
+    'ondoc.ckedit',
+    'ondoc.integrations'
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
