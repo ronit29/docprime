08280b20745d459ceecc42fa9399622b3ac2ee9a
shashanksingh <shashanks@policybazaar.com>
2019-01-23 16:51:20 +0530

    added model and validators

--- ondoc/diagnostic/models.py
+++ ondoc/diagnostic/models.py
@@ -795,6 +795,21 @@ class LabTestCategoryMapping(models.Model):
         unique_together = (('lab_test', 'parent_category'),)
 
 
+class LabTestRecommendedCategoryMapping(models.Model):
+    lab_test = models.ForeignKey('LabTest', on_delete=models.CASCADE,
+                                 related_name='recommended_lab_test_category_mappings')
+    parent_category = models.ForeignKey(LabTestCategory, on_delete=models.CASCADE,
+                                        related_name='recommended_lab_test_mappings')
+    show_on_recommended_screen = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '({}){}'.format(self.lab_test, self.parent_category)
+
+    class Meta:
+        db_table = "lab_test_recommended_category_mapping"
+        unique_together = (('lab_test', 'parent_category'),)
+
+
 class LabTest(TimeStampedModel, SearchKey):
     RADIOLOGY = 1
     PATHOLOGY = 2
@@ -843,19 +858,23 @@ class LabTest(TimeStampedModel, SearchKey):
                                         through=LabTestCategoryMapping,
                                         through_fields=('lab_test', 'parent_category'),
                                         related_name='lab_tests')
-    min_age = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
-    max_age = models.PositiveSmallIntegerField(default=None, blank=True, null=True)
+    min_age = models.PositiveSmallIntegerField(default=None, blank=True, null=True, validators=[MaxValueValidator(120), MinValueValidator(1)])
+    max_age = models.PositiveSmallIntegerField(default=None, blank=True, null=True, validators=[MaxValueValidator(120), MinValueValidator(1)])
     MALE = 1
     FEMALE = 2
     ALL = 3
     GENDER_TYPE_CHOICES = (
-	    ('', 'Select'),
+        ('', 'Select'),
         (MALE, 'male'),
         (FEMALE, 'female'),
         (ALL, 'all')
     )
     gender_type = models.PositiveIntegerField(choices=GENDER_TYPE_CHOICES, blank=True, null=True)
-
+    recommended_categories = models.ManyToManyField(LabTestCategory,
+                                        through=LabTestRecommendedCategoryMapping,
+                                        through_fields=('lab_test', 'parent_category'),
+                                        related_name='recommended_lab_tests')
+    show_on_recommended_screen = models.BooleanField(default=False)
     # test_sub_type = models.ManyToManyField(
     #     LabTestSubType,
     #     through='LabTestSubTypeMapping',
