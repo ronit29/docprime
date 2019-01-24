f1d0a987d2610110369dca20b00a1b30fe56d69e
Akash Saini <akashs@docprime.com>
2019-01-24 15:38:36 +0530

    interim commit

--- ondoc/crm/admin/integrations.py
+++ ondoc/crm/admin/integrations.py
@@ -4,5 +4,7 @@ from ondoc.integrations.models import IntegratorMapping
 
 class IntegratorMappingAdmin(admin.ModelAdmin):
     model = IntegratorMapping
-    list_display = ('integrator_class_name', 'test', 'content_object')
-    fields = ('test', 'integrator_test_name')
+    list_display = ('integrator_class_name', 'integrator_test_name', 'is_active',)
+    fields = ('test', 'integrator_test_name', 'is_active')
+
+    readonly_fields = ('integrator_test_name', )
