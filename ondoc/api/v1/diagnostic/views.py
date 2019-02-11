d426355807aa2d4726d0f4f3d47dc4b29d29f01d
shreyas <shreyassridhar@docprime.com>
2019-02-11 14:42:40 +0530

    fixes

--- ondoc/api/v1/diagnostic/views.py
+++ ondoc/api/v1/diagnostic/views.py
@@ -817,17 +817,32 @@ class LabList(viewsets.ReadOnlyModelViewSet):
         tests = dict()
         lab = dict()
 
+        # for test in LabTest.objects.filter(id__in=ids):
+        #         if test.categories.all():
+        #             selected_categories = test.get_all_categories_detail()
+        # if test_ids:
+        #     for test in LabTest.objects.filter(id__in=test_ids):
+        #         if test.categories.all():
+        #             selected_categories = test.get_all_categories_detail()
+        #             test[obj.id].append({"categories": selected_categories})
+
+
         for obj in labs:
             temp_var[obj.id] = obj
             tests[obj.id] = list()
+            # res = []
+            # for test in LabTest.objects.filter(id__in=test_ids):
+            #     if test.categories.all():
+            #         selected_categories = test.get_all_categories_detail()
+            #         res.append(selected_categories)
+
             if test_ids and obj.selected_group and obj.selected_group.selected_tests:
                 for test in obj.selected_group.selected_tests:
                     if test.custom_deal_price:
                         deal_price=test.custom_deal_price
                     else:
                         deal_price=test.computed_deal_price
-                    tests[obj.id].append({"id": test.test_id, "name": test.test.name, "deal_price": deal_price, "mrp": test.mrp,
-                                          "number_of_tests": test.test.number_of_tests})
+                    tests[obj.id].append({"id": test.test_id, "name": test.test.name, "deal_price": deal_price, "mrp": test.mrp, "number_of_tests": test.test.number_of_tests, 'categories': test.test.get_all_categories_detail()})
 
         # day_now = timezone.now().weekday()
         # days_array = [i for i in range(7)]
