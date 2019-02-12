e39967463105204861cd6b2c52cd58842752d4fa
Mayank <mayankgupta@docprime.com>
2019-02-12 13:56:22 +0530

    autocomplete field

--- ondoc/crm/admin/integrations.py
+++ ondoc/crm/admin/integrations.py
@@ -7,13 +7,13 @@ class IntegratorMappingAdmin(admin.ModelAdmin):
     model = IntegratorMapping
     list_display = ('integrator_class_name', 'integrator_test_name', 'is_active',)
     fields = ('test', 'integrator_test_name', 'is_active',)
-
     readonly_fields = ('integrator_test_name',)
+    autocomplete_fields = ['test']
 
 
 class IntegratorProfileMappingAdmin(admin.ModelAdmin):
     model = IntegratorProfileMapping
     list_display = ('integrator_class_name', 'integrator_package_name', 'is_active',)
     fields = ('package', 'integrator_package_name', 'is_active',)
-
     readonly_fields = ('integrator_package_name',)
+    autocomplete_fields = ['package']
