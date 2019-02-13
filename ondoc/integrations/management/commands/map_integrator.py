e575312a00f803c62e35a200a9f2358f0827302c
Mayank <mayankgupta@docprime.com>
2019-02-13 19:10:59 +0530

    network id

--- ondoc/integrations/management/commands/map_integrator.py
+++ ondoc/integrations/management/commands/map_integrator.py
@@ -1,9 +1,10 @@
 from django.core.management import BaseCommand
 from ondoc.integrations.Integrators import Thyrocare
+from django.conf import settings
 
 
 def map_integrators():
-    obj_id = 1
+    obj_id = settings.THYROCARE_NETWORK_ID
     Thyrocare.thyrocare_product_data(obj_id, 'TESTS')
     Thyrocare.thyrocare_profile_data(obj_id, 'PROFILE')
 
