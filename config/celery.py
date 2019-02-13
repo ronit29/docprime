fb7e280856f54db895c1d5dbb27b8c45f14f3686
ronit <ronitd@policybazaar.com>
2019-02-13 18:46:17 +0530

    celery updates2

--- config/celery.py
+++ config/celery.py
@@ -56,6 +56,6 @@ app.autodiscover_tasks()
 def setup_periodic_tasks(sender, **kwargs):
     polling_time = float(settings.PG_REFUND_STATUS_POLL_TIME) * float(60.0)
     sender.add_periodic_task(polling_time, consumer_refund_update.s(), name='Refund and update consumer account balance')
-    sender.add_periodic_task(crontab(minute=0, hour=0), save_avg_rating.s(), name='Update Lab and Doctor Average Rating')
+    sender.add_periodic_task(crontab(hour=18, minute=30), save_avg_rating.s(), name='Update Lab and Doctor Average Rating')
     # doctor_search_score_creation_time = float(settings.CREATE_DOCTOR_SEARCH_SCORE) * float(3600.0)
     # sender.add_periodic_task(doctor_search_score_creation_time, create_search_score.s(), name='Doctor search score updaed')
