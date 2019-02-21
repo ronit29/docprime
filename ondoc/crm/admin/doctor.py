fed1528e19a49a1b4ddf88524928802a7fc9295d
shashanksingh <shashanks@policybazaar.com>
2019-02-21 13:46:29 +0530

    minor fix doctor admin

--- ondoc/crm/admin/doctor.py
+++ ondoc/crm/admin/doctor.py
@@ -681,16 +681,20 @@ class DoctorForm(FormCleanMixin):
             return
         data = self.cleaned_data
         is_enabled = data.get('enabled', None)
+        enabled_for_online_booking = data.get('enabled_for_online_booking', None)
         if is_enabled is None:
             is_enabled = self.instance.enabled if self.instance else False
-        if is_enabled:
+        if enabled_for_online_booking is None:
+            enabled_for_online_booking = self.instance.enabled_for_online_booking if self.instance else False
+        if is_enabled and enabled_for_online_booking:
             if any([data.get('disabled_after', None), data.get('disable_reason', None),
                     data.get('disable_comments', None)]):
                 raise forms.ValidationError(
-                    "Cannot have disabled after/disabled reason/disable comments if doctor is enabled.")
-        else:
+                    "Cannot have disabled after/disabled reason/disable comments if doctor is enabled or not enabled for online booking.")
+        elif not is_enabled or not enabled_for_online_booking:
             if not all([data.get('disabled_after', None), data.get('disable_reason', None)]):
-                raise forms.ValidationError("Must have disabled after/disable reason if doctor is not enabled.")
+                raise forms.ValidationError(
+                    "Must have disabled after/disable reason if doctor is not enabled or not enabled for online booking.")
             if data.get('disable_reason', None) and data.get('disable_reason', None) == Doctor.OTHERS and not data.get(
                     'disable_comments', None):
                 raise forms.ValidationError("Must have disable comments if disable reason is others.")
