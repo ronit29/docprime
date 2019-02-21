6db785c657eb289a08ea6311f09b3194e6466bb0
Akash Saini <akashs@docprime.com>
2019-02-21 18:38:24 +0530

    elastic sync

--- config/celery.py
+++ config/celery.py
@@ -6,6 +6,7 @@ import os
 from django.conf import settings
 from raven.contrib.celery import register_signal, register_logger_signal
 from ondoc.account.tasks import refund_status_update, consumer_refund_update
+from celery.schedules import crontab
 # from ondoc.doctor.services.update_search_score import DoctorSearchScore
 
 
@@ -56,6 +57,9 @@ def setup_periodic_tasks(sender, **kwargs):
     polling_time = float(settings.PG_REFUND_STATUS_POLL_TIME) * float(60.0)
     sync_elastic_polling_time = float(settings.SYNC_ELASTIC) * float(60.0)
     sender.add_periodic_task(polling_time, consumer_refund_update.s(), name='Refund and update consumer account balance')
-    sender.add_periodic_task(sync_elastic_polling_time, dump_to_elastic.s(), name='Sync Elastic')
+
+    elastic_sync_cron_schedule = crontab(hour=1)
+
+    sender.add_periodic_task(elastic_sync_cron_schedule, dump_to_elastic.s(), name='Sync Elastic')
     # doctor_search_score_creation_time = float(settings.CREATE_DOCTOR_SEARCH_SCORE) * float(3600.0)
     # sender.add_periodic_task(doctor_search_score_creation_time, create_search_score.s(), name='Doctor search score updaed')
