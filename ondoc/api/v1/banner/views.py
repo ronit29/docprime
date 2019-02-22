42b7113e23bc0e0b8eda7fa5a5b1926ecaa9594a
shreyas <shreyassridhar@docprime.com>
2019-02-22 17:59:57 +0530

    fixes

--- ondoc/api/v1/banner/views.py
+++ ondoc/api/v1/banner/views.py
@@ -13,30 +13,34 @@ class BannerListViewSet(viewsets.GenericViewSet):
 
     def list(self, request):
         parameters = request.query_params
-        lat = parameters.get('lat', '28.450367')
-        long = parameters.get('long', '77.071848')
+        lat = parameters.get('lat', None)
+        long = parameters.get('long', None)
         banners = Banner.get_all_banners(request)
         res = []
         for banner_obj in banners:
-            if banner_obj.get('latitude') and banner_obj.get('longitude') and banner_obj.get('radius'):
-                latitude = banner_obj.get('latitude')
-                longitude = banner_obj.get('longitude')
-                radius = banner_obj.get('radius') * 1000  # Radius in metres
-                pnt1 = Point(float(longitude), float(latitude))
-                if lat and long:
-                    try:
-                        pnt2 = Point(float(long), float(lat))
-                    except:
-                        return Response({'msg': 'Invalid Lat Long'}, status=status.HTTP_400_BAD_REQUEST)
+            if lat and long:
+                if banner_obj.get('latitude') and banner_obj.get('longitude') and banner_obj.get('radius'):
+                    latitude = banner_obj.get('latitude')
+                    longitude = banner_obj.get('longitude')
+                    radius = banner_obj.get('radius') * 1000  # Radius in metres
+                    pnt1 = Point(float(longitude), float(latitude))
+                    if lat and long:
+                        try:
+                            pnt2 = Point(float(long), float(lat))
+                        except:
+                            return Response({'msg': 'Invalid Lat Long'}, status=status.HTTP_400_BAD_REQUEST)
 
-                    distance = pnt1.distance(pnt2)*100  # Distance in metres
-                    if distance > radius:
+                        distance = pnt1.distance(pnt2)*100  # Distance in metres
+                        if distance > radius:
 
-                        continue
+                            continue
 
-                    else:
+                        else:
 
-                        res.append(banner_obj)
+                            res.append(banner_obj)
+
+                else:
+                    res.append(banner_obj)
             else:
                 res.append(banner_obj)
 
