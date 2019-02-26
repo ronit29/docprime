6cbd2e26547166da262045ab80c81e1148152c2c
Arun Chaudhary <arunchaudhary@policybazaar.com>
2019-02-26 18:58:25 +0530

    fixes

--- ondoc/crm/management/commands/setuppermissions.py
+++ ondoc/crm/management/commands/setuppermissions.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 from django.db.models import Q
 
-from ondoc.banner.models import Banner
+from ondoc.banner.models import Banner, SliderLocation
 from ondoc.common.models import PaymentOptions, UserConfig
 from ondoc.coupon.models import Coupon, UserSpecificCoupon
 from ondoc.crm.constants import constants
@@ -474,7 +474,7 @@ class Command(BaseCommand):
         group, created = Group.objects.get_or_create(name=constants['PRODUCT_TEAM'])
         group.permissions.clear()
 
-        content_types = ContentType.objects.get_for_models(LabTestRecommendedCategoryMapping, Banner, UserConfig, NewDynamic, QuestionAnswer, FrequentlyAddedTogetherTests)
+        content_types = ContentType.objects.get_for_models(LabTestRecommendedCategoryMapping, Banner,SliderLocation, UserConfig, NewDynamic, QuestionAnswer, FrequentlyAddedTogetherTests)
         for cl, ct in content_types.items():
             permissions = Permission.objects.filter(
                 Q(content_type=ct),
