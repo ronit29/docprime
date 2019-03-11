47d2074f3aea04300d7c705401387a33f0f1c755
navneetdp <navneetsingh@docprime.com>
2019-03-11 16:27:13 +0530

    minor change

--- ondoc/api/v1/doctor/views.py
+++ ondoc/api/v1/doctor/views.py
@@ -1023,7 +1023,8 @@ class PrescriptionFileViewset(OndocViewSet):
         # resp_data = prescription_file_serializer.data
         resp_data = serializers.DoctorAppointmentRetrieveSerializer(validated_data.get('appointment'),
                                                                          context={'request': request}).data
-        resp_data['prescriptions'] = validated_data.get('appointment').get_prescriptions(request)
+        if validated_data.get('appointment'):
+            resp_data['prescriptions'] = validated_data.get('appointment').get_prescriptions(request)
 
         return Response(resp_data)
 
