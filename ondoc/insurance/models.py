b3b2915a2a405b3961da087ee38f356ecd443d41
Akash Saini <akashs@docprime.com>
2019-03-13 13:46:07 +0530

    is_selected flag

--- ondoc/insurance/models.py
+++ ondoc/insurance/models.py
@@ -119,6 +119,7 @@ class InsurancePlans(auth_model.TimeStampedModel, LiveMixin):
     enabled = models.BooleanField(default=False)
     is_live = models.BooleanField(default=False)
     total_allowed_members = models.PositiveSmallIntegerField(default=0)
+    is_selected = models.BooleanField(default=False)
 
     @property
     def get_active_threshold(self):
