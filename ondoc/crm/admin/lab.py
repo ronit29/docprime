31818b54bf2b7f527155e2242843dd840e380baa
Arun Chaudhary <arunchaudhary@policybazaar.com>
2019-03-18 19:31:23 +0530

    show if appointment booked is via app

--- ondoc/crm/admin/lab.py
+++ ondoc/crm/admin/lab.py
@@ -800,6 +800,9 @@ class LabAppointmentAdmin(nested_admin.NestedModelAdmin):
     #         temp_autocomplete_fields = super().get_autocomplete_fields(request)
     #     return temp_autocomplete_fields
 
+    def through_app(self, obj):
+        return obj.created_by_native()
+
     def get_profile_email(self, obj):
         if not obj.profile:
             return None
@@ -861,7 +864,7 @@ class LabAppointmentAdmin(nested_admin.NestedModelAdmin):
         #             'send_email_sms_report', 'invoice_urls', 'reports_uploaded', 'email_notification_timestamp', 'payment_type'
         #             )
         # elif request.user.groups.filter(name=constants['LAB_APPOINTMENT_MANAGEMENT_TEAM']).exists():
-            return ('booking_id', 'order_id',  'lab_id', 'lab_name', 'get_lab_test', 'lab_contact_details',
+            return ('booking_id', 'through_app', 'order_id',  'lab_id', 'lab_name', 'get_lab_test', 'lab_contact_details',
                     'used_profile_name', 'used_profile_number',
                     'default_profile_name', 'default_profile_number', 'user_id', 'user_number', 'price', 'agreed_price',
                     'deal_price', 'effective_price', 'payment_status', 'payment_type', 'insurance', 'is_home_pickup',
@@ -876,7 +879,7 @@ class LabAppointmentAdmin(nested_admin.NestedModelAdmin):
         # if request.user.is_superuser:
         #     read_only =  ['booking_id', 'order_id', 'lab_id', 'lab_contact_details', 'get_lab_test', 'invoice_urls', 'reports_uploaded', 'email_notification_timestamp', 'payment_type']
         # elif request.user.groups.filter(name=constants['LAB_APPOINTMENT_MANAGEMENT_TEAM']).exists():
-        read_only = ['booking_id', 'order_id', 'lab_name', 'lab_id', 'get_lab_test', 'invoice_urls',
+        read_only = ['booking_id','through_app', 'order_id', 'lab_name', 'lab_id', 'get_lab_test', 'invoice_urls',
                 'lab_contact_details', 'used_profile_name', 'used_profile_number',
                 'default_profile_name', 'default_profile_number', 'user_number', 'user_id', 'price', 'agreed_price',
                 'deal_price', 'effective_price', 'payment_status', 'otp',
