f92c8cb52e2370f8897121f9d93d3ec936361bea
shashanksingh <shashanks@policybazaar.com>
2019-03-26 10:54:18 +0530

    fix

--- ondoc/crm/admin/lab.py
+++ ondoc/crm/admin/lab.py
@@ -745,23 +745,24 @@ class LabAppointmentForm(forms.ModelForm):
         if not lab.lab_pricing_group:
             raise forms.ValidationError("Lab is not in any lab pricing group.")
 
-        if self.instance.id:
-            selected_test_ids = lab_test.values_list('test', flat=True)
-            is_lab_timing_available = LabTiming.objects.filter(
-                lab=lab,
-                lab__lab_pricing_group__available_lab_tests__test__in=selected_test_ids,
-                day=time_slot_start.weekday(),
-                start__lte=hour, end__gt=hour).exists()
-            # if not is_lab_timing_available:
-            #     raise forms.ValidationError("This lab test is not available on selected day and time.")
-            if self.instance.is_home_pickup or cleaned_data.get('is_home_pickup'):
-                if not lab.is_home_collection_enabled:
-                    raise forms.ValidationError("Home Pickup is disabled for the lab")
-                if hour < 7.0 or hour > 19.0:
-                    raise forms.ValidationError("No time slot available")
-            else:
-                if not lab.always_open and not is_lab_timing_available:
-                    raise forms.ValidationError("No time slot available")
+        if cleaned_data.get('status') not in [LabAppointment.CANCELLED, LabAppointment.COMPLETED, None]:
+            if self.instance.id:
+                selected_test_ids = lab_test.values_list('test', flat=True)
+                is_lab_timing_available = LabTiming.objects.filter(
+                    lab=lab,
+                    lab__lab_pricing_group__available_lab_tests__test__in=selected_test_ids,
+                    day=time_slot_start.weekday(),
+                    start__lte=hour, end__gt=hour).exists()
+                # if not is_lab_timing_available:
+                #     raise forms.ValidationError("This lab test is not available on selected day and time.")
+                if self.instance.is_home_pickup or cleaned_data.get('is_home_pickup'):
+                    if not lab.is_home_collection_enabled:
+                        raise forms.ValidationError("Home Pickup is disabled for the lab")
+                    if hour < 7.0 or hour > 19.0:
+                        raise forms.ValidationError("No time slot available")
+                else:
+                    if not lab.always_open and not is_lab_timing_available:
+                        raise forms.ValidationError("No time slot available")
 
         return cleaned_data
 
