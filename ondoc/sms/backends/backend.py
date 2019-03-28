f4f9028f98658845f64f86ef0b6922e084377101
shashanksingh <shashanks@policybazaar.com>
2019-03-28 19:43:07 +0530

    fix

--- ondoc/sms/backends/backend.py
+++ ondoc/sms/backends/backend.py
@@ -12,12 +12,11 @@ from dateutil.relativedelta import relativedelta
 class NodeJsSmsBackend(object):
 
     def send(self, message, phone_no, retry_send=False):
-        from requests.utils import quote
         payload = {
             "type": "sms",
             "data": {
                 "phone_number": phone_no,
-                "content": quote(message),
+                "content": message,
                 "retry": retry_send
             }
         }
