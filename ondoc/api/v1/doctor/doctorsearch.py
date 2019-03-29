107c925c95f3ceeb8f10ccaa10499cc5da60f64e
Sonam Sinha <sonamsinha@policybazaar.com>
2019-03-29 16:15:59 +0530

    fixes

--- ondoc/api/v1/doctor/doctorsearch.py
+++ ondoc/api/v1/doctor/doctorsearch.py
@@ -164,12 +164,18 @@ class DoctorSearchHelper:
             search_key = " ".join(search_key).lower()
 
             search_key = "".join(search_key.split("."))
-            filtering_params.append("d.search_key like concat((%(doctor_name)s),' %') or d.search_key like concat('% ',%(doctor_name)s, ' %')  or d.search_key like concat('% ',%(doctor_name)s) ")
+            filtering_params.append("d.search_key like (%(doctor_name1)s) "
+                                    "or d.search_key like  %(doctor_name2)s "
+                                    "or d.search_key like %(doctor_name3)s ")
             # filtering_params.append(
             #     "d.search_key ilike (%(doctor_name)s)"
             #         )
             # params['doctor_name'] = '%'+search_key+'%'
-            params['doctor_name'] = search_key
+            params['order_doctor'] = search_key
+            params['doctor_name1'] = search_key + ' %'
+            params['doctor_name2'] = '% ' + search_key + ' %'
+            params['doctor_name3'] = '% ' + search_key
+
         if self.query_params.get("hospital_name"):
             search_key = re.findall(r'[a-z0-9A-Z.]+', self.query_params.get("hospital_name"))
             search_key = " ".join(search_key).lower()
@@ -194,6 +200,8 @@ class DoctorSearchHelper:
     def get_ordering_params(self):
         # order_by_field = 'is_gold desc, distance, dc.priority desc'
         # rank_by = "rank_distance=1"
+        if self.query_params and self.query_params.get('doctor_name'):
+             return 'position(%(order_doctor)s in search_key), enabled_for_online_booking DESC', ' rnk=1'
 
         if self.query_params.get('url') and (not self.query_params.get('sort_on') \
                                              or self.query_params.get('sort_on')=='distance'):
@@ -265,9 +273,6 @@ class DoctorSearchHelper:
 
         specialization_ids = self.query_params.get("specialization_ids", [])
         condition_ids = self.query_params.get("condition_ids", [])
-
-        if self.query_params.get('doctor_name'):
-            order_by_field = "{}, {} ".format('search_key ASC ', order_by_field)
 
         if self.count_of_procedure:
             rank_part = "Row_number() OVER( PARTITION BY doctor_id ORDER BY " \
