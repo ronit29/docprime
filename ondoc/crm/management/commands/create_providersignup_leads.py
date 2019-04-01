b294b180318417cbd8162b4ed1014c9eb23628d7
navneetdp <navneetsingh@docprime.com>
2019-04-01 18:53:53 +0530

    management command for back-populating matrix leads providersignup

--- ondoc/crm/management/commands/create_providersignup_leads.py
+++ ondoc/crm/management/commands/create_providersignup_leads.py
@@ -1,21 +1,24 @@
 from django.core.management.base import BaseCommand
-from ondoc.doctor.models import OpdAppointment
-from ondoc.diagnostic.models import LabAppointment
+from ondoc.doctor.models import ProviderSignupLead
+from ondoc.diagnostic.models import LabAppointment, Hospital
+from ondoc.matrix.tasks import create_or_update_lead_on_matrix
 
 
 class Command(BaseCommand):
-    help = 'Send a test mail using email send api'
+    help = 'Create matrix leads for provider signup data in database without matrix lead id'
 
     def handle(self, *args, **options):
 
-        for o in OpdAppointment.objects.filter(status=7).order_by('-id'):
+        for p in ProviderSignupLead.objects.filter(is_docprime=True, matrix_lead_id=None).order_by('-id'):
             try:
-                o.generate_invoice()
+                create_or_update_lead_on_matrix.apply_async(({'obj_type': p.__class__.__name__, 'obj_id': p.id}
+                                                             ,), countdown=5)
             except Exception as e:
-                print("Some error occured for OPD appointment with ID-{}. ERROR :: {}".format(o.id, str(e)))
+                print("Some error occured for ProviderSignupLead with ID-{}. ERROR :: {}".format(p.id, str(e)))
 
-        for l in LabAppointment.objects.filter(status=7).order_by('-id'):
+        for h in Hospital.objects.filter(is_listed_on_docprime=True, source_type=Hospital.PROVIDER).order_by('-id'):
             try:
-                l.generate_invoice()
+                create_or_update_lead_on_matrix.apply_async(({'obj_type': h.__class__.__name__, 'obj_id': h.id}
+                                                             ,), countdown=5)
             except Exception as e:
-                print("Some error occured for LAB appointment with ID-{}. ERROR :: {}".format(l.id, str(e)))
+                print("Some error occured for Hospital with ID-{}. ERROR :: {}".format(h.id, str(e)))
