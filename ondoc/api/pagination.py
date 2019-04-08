8aa163f6917294527bcccced71c75363d377c93f
Arun Chaudhary <arunchaudhary@policybazaar.com>
2019-04-08 17:18:37 +0530

    fixes

--- ondoc/api/pagination.py
+++ ondoc/api/pagination.py
@@ -1,5 +1,4 @@
 def paginate_queryset(queryset, request, page_size=20):
-
     page = int(request.query_params.get('page', 1))
 
     if page_size < 0 or page_size > 100:
@@ -23,5 +22,3 @@ def paginate_raw_query(request, query_string, page_size=20):
 
     offset = (page - 1) * page_size
     return " {} offset {} limit {}".format(query_string, offset, page_size)
-
-
