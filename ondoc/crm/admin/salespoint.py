dd0d12ee8b67b0bd6d8f9b1c1d6ac55e2f0ec673
Akash Saini <akashs@docprime.com>
2019-04-10 14:33:17 +0530

    interim commit

--- ondoc/crm/admin/salespoint.py
+++ ondoc/crm/admin/salespoint.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+
+from ondoc.diagnostic.models import AvailableLabTest
 from ondoc.salespoint.models import SalesPoint, SalespointTestmapping
 from django import forms
 
@@ -11,3 +13,10 @@ class SalesPointAdmin(admin.ModelAdmin):
 class SalesPointAvailableTestMappingAdmin(admin.ModelAdmin):
     model = SalespointTestmapping
     list_display = ['salespoint', 'available_tests']
+
+    def get_form(self, request, obj=None, **kwargs):
+
+        form = super(SalesPointAvailableTestMappingAdmin, self).get_form(request, obj=obj, **kwargs)
+        form.base_fields['available_tests'].queryset = AvailableLabTest.objects.filter(test__is_package=True)
+
+        return form
