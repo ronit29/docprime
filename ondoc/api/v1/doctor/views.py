dfe066e7f39dd1826ec6fd17afde26cf898cbd1c
shashanksingh <shashanks@policybazaar.com>
2019-04-11 12:30:09 +0530

    multiply

--- ondoc/api/v1/doctor/views.py
+++ ondoc/api/v1/doctor/views.py
@@ -3413,7 +3413,7 @@ class HospitalViewSet(viewsets.GenericViewSet):
         if min_distance:
             hospital_queryset = filter(lambda x: x.distance.m >= min_distance if x.distance is not None and x.distance.m is not None else False, hospital_queryset)
 
-        hospital_queryset = list(hospital_queryset)
+        hospital_queryset = list(hospital_queryset) * 30 # SHASHANK_SINGH Remove this muliplication only for testing purpose
         result_count = len(hospital_queryset)
         if count:
             hospital_queryset = hospital_queryset[:count]
