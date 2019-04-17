18995b373846c5fbfab7dc2d5b93b3a688b1373d
Mayank <mayankgupta@docprime.com>
2019-04-17 13:37:24 +0530

    url fix

--- ondoc/notification/models.py
+++ ondoc/notification/models.py
@@ -850,7 +850,7 @@ class SmsNotification(TimeStampedModel, SmsNotificationOpdMixin, SmsNotification
 
     @classmethod
     def send_cart_url(cls, token, phone_number, order_id):
-        callback_url = "{}/payment/{}?refs=lab".format(settings.CONSUMER_APP_DOMAIN, order_id)
+        callback_url = "payment/{}?refs=lab".format(order_id)
         payment_page_url = "{}/agent/booking?token={}&agent=false&callbackurl={}".format(settings.CONSUMER_APP_DOMAIN, token, callback_url)
         # short_url = generate_short_url(payment_page_url)
         html_body = "Your booking url is - {} . Please pay to confirm".format(payment_page_url)
