7c6eadafc1789c2829c61b9d21b68c2acd19f88f
Sonam Sinha <sonamsinha@policybazaar.com>
2019-04-19 12:14:25 +0530

    fixes

--- ondoc/authentication/models.py
+++ ondoc/authentication/models.py
@@ -1481,7 +1481,7 @@ class Merchant(TimeStampedModel):
     def save(self, *args, **kwargs):
         if self.verified_by_finance and (self.pg_status == self.NOT_INITIATED or self.pg_status == self.FAILURE):
             pass
-            self.create_in_pg()
+            # self.create_in_pg()
 
         super().save(*args, **kwargs)
 
