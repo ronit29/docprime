ad7738581b2f6bba17279233b603e533e30586a2
Akash Saini <akashs@docprime.com>
2019-04-29 11:48:32 +0530

    adding integration model to setup permissions

--- ondoc/crm/management/commands/setuppermissions.py
+++ ondoc/crm/management/commands/setuppermissions.py
@@ -51,7 +51,7 @@ from ondoc.procedure.models import Procedure, ProcedureCategory, CommonProcedure
 from ondoc.reports import models as report_models
 
 from ondoc.diagnostic.models import LabPricing
-from ondoc.integrations.models import IntegratorMapping, IntegratorProfileMapping, IntegratorReport, IntegratorTestMapping
+from ondoc.integrations.models import IntegratorMapping, IntegratorProfileMapping, IntegratorReport, IntegratorTestMapping, IntegratorTestParameterMapping
 from ondoc.subscription_plan.models import Plan, PlanFeature, PlanFeatureMapping, UserPlanMapping
 
 from ondoc.web.models import Career, OnlineLead, UploadImage
@@ -561,7 +561,7 @@ class Command(BaseCommand):
         group.permissions.clear()
 
         content_types = ContentType.objects.get_for_models(IntegratorMapping, IntegratorProfileMapping, LabTest, LabNetwork,
-                                                           IntegratorReport, IntegratorTestMapping)
+                                                           IntegratorReport, IntegratorTestMapping, IntegratorTestParameterMapping)
 
         for cl, ct in content_types.items():
             permissions = Permission.objects.filter(
