ca4cb021e1fba89ed7e4ceba6b99484970b5c558
Akash Saini <akashs@docprime.com>
2019-05-20 16:01:39 +0530

    interim commit

--- ondoc/crm/admin/insurance.py
+++ ondoc/crm/admin/insurance.py
@@ -1076,10 +1076,12 @@ class EndorsementRequestAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         user = request.user
         obj._user = user if user and not user.is_anonymous else None
+
         if request.user.is_member_of(constants['SUPER_INSURANCE_GROUP']) or request.user.is_member_of(constants['INSURANCE_GROUP']):
+            super().save_model(request, obj, form, change)
+
             if obj.status == EndorsementRequest.APPROVED:
                 obj.process_endorsement()
-                super().save_model(request, obj, form, change)
             elif obj.status == EndorsementRequest.REJECT:
                 obj.reject_endorsement()
             if obj.mail_coi_to_customer and obj.status == EndorsementRequest.APPROVED:
