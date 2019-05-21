f30e34c78040760a898590a3f31af3788b881eaa
Arun Chaudhary <arunchaudhary@policybazaar.com>
2019-05-21 14:37:26 +0530

    fixes

--- ondoc/api/v1/diagnostic/serializers.py
+++ ondoc/api/v1/diagnostic/serializers.py
@@ -541,17 +541,22 @@ class CommonPackageSerializer(serializers.ModelSerializer):
 
     def __init__(self, instance=None, data=None, **kwargs):
         super().__init__(instance, data, **kwargs)
-        queryset = AvailableLabTest.objects.all()
+        queryset = AvailableLabTest.objects
         filters = Q()  # Create an empty Q object to start with
 
         for ins in instance:
-            q = Q(lab_pricing_group_id=ins.lab.lab_pricing_group_id,test_id = ins.package_id)
-            filters |= q
-        queryset = queryset.filter(filters)
+            if ins.lab and ins.lab.lab_pricing_group_id and ins.package_id:
+                q = Q(lab_pricing_group_id=ins.lab.lab_pricing_group_id,test_id = ins.package_id)
+                filters |= q
+
+        if filters:
+            queryset = queryset.filter(filters)
+        else:
+            queryset = queryset.none()
         for ins in instance:
             ins._selected_test = None
             for x in queryset:
-                if ins.lab.lab_pricing_group_id == x.lab_pricing_group_id and ins.package_id == x.test_id:
+                if ins.lab and ins.lab.lab_pricing_group_id == x.lab_pricing_group_id and ins.package_id == x.test_id:
                     ins._selected_test = x
                     break
 
