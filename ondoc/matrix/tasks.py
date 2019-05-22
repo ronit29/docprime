74a1cfb371ff46492798d07c24bfc3f4aec9448f
shashanksingh <shashanks@policybazaar.com>
2019-05-22 12:53:32 +0530

    fix

--- ondoc/matrix/tasks.py
+++ ondoc/matrix/tasks.py
@@ -584,9 +584,7 @@ def create_or_update_lead_on_matrix(self, data):
         model_used = ct.model_class()
         content_type = ContentType.objects.get_for_model(model_used)
         if obj_type == ProviderSignupLead.__name__:
-            exit_point_url = settings.ADMIN_BASE_URL + reverse('admin:doctor_doctor_add')
-        elif obj_type == IpdProcedureLead.__name__:
-            exit_point_url = ''
+            exit_point_url = settings.ADMIN_BASE_URL + reverse('admin:doctor_doctor_add')gi
         else:
             exit_point_url = settings.ADMIN_BASE_URL + reverse(
                 'admin:{}_{}_change'.format(content_type.app_label, content_type.model), kwargs={"object_id": obj_id})
