bfb23f0e34fa3a8d2daf4d535e8939d623b7fc7f
Akash Saini <akashs@docprime.com>
2019-06-20 13:18:10 +0530

    bank holidays  and insurance

--- ondoc/insurance/models.py
+++ ondoc/insurance/models.py
@@ -510,6 +510,17 @@ class InsurerPolicyNumber(auth_model.TimeStampedModel):
         return InsurerAccount.objects.order_by('id').first()
 
 
+class BankHolidays(auth_model.TimeStampedModel):
+    date = models.DateField(null=False, blank=False)
+
+    @classmethod
+    def is_holiday(cls, date):
+        return cls.objects.filter(date=date).exists()
+
+    class Meta:
+        db_table = "bank_holidays"
+
+
 class UserInsurance(auth_model.TimeStampedModel):
     from ondoc.account.models import MoneyPool
 
@@ -562,6 +573,33 @@ class UserInsurance(auth_model.TimeStampedModel):
 
     def __str__(self):
         return str(self.user)
+
+
+    @property
+    def hours_elapsed(self):
+        hours = 0
+        current_datetime = timezone.now()
+        created_at = self.created_at
+
+        if not BankHolidays.is_holiday(created_at.date()):
+            temp_datetime = created_at + timedelta(days=1)
+            temp_datetime = temp_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+            hours += ((temp_datetime - created_at).total_seconds()) // 3600
+        else:
+            temp_datetime = created_at + timedelta(days=1)
+            temp_datetime = temp_datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+
+        while temp_datetime.date() < current_datetime.date():
+            if not BankHolidays.is_holiday(temp_datetime.date()):
+                hours += 24
+
+            temp_datetime = temp_datetime + timedelta(days=1)
+
+        if not BankHolidays.is_holiday(current_datetime.date()):
+            hours += ((current_datetime - temp_datetime).total_seconds()) // 3600
+
+        return math.floor(hours)
+
 
     @cached_property
     def master_policy(self):
@@ -1607,6 +1645,12 @@ class UserInsurance(auth_model.TimeStampedModel):
     #         # Order not processed but payment sucess and same wallet amount used next time.
     #         if len(pg_transactions) == 1 and pg_transactions.first():
     #             pass
+
+    @classmethod
+    def transfer_to_nodal_if_required(cls):
+        objs = cls.objects.filter(premium_transferred=False)
+        for obj in objs:
+            obj.transfer_to_insurance_nodal()
 
     def is_bank_details_exist(self):
         bank_obj = UserBank.objects.filter(insurance=self).order_by('-id').first()
