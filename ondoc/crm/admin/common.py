4c12374cb02234f9c6cec7cbc82b91032037d507
Sonam Sinha <sonamsinha@policybazaar.com>
2019-06-25 16:38:24 +0530

    fixes

--- ondoc/crm/admin/common.py
+++ ondoc/crm/admin/common.py
@@ -140,14 +140,15 @@ class FormCleanMixin(forms.ModelForm):
         self.pin_code_qc_submit()
         phone_no_flag = False
         if isinstance(self.instance, Hospital) and self.data and self.instance.network_type == 1:
-            total_forms = int(self.data.get('authentication-spocdetails-content_type-object_id-TOTAL_FORMS'))
-            for form in range(total_forms):
-                number_pattern = re.compile("(0/91)?[6-9][0-9]{9}")
-                phone_number = self.data.get('authentication-spocdetails-content_type-object_id-' + str(form) + '-number')
-                if phone_number and number_pattern.match(str(phone_number)):
-                    phone_no_flag = True
-            if phone_no_flag == False:
-                raise forms.ValidationError("Atleast one mobile no is required for SPOC Details")
+            if self.data.get('authentication-spocdetails-content_type-object_id-TOTAL_FORMS'):
+                total_forms = int(self.data.get('authentication-spocdetails-content_type-object_id-TOTAL_FORMS'))
+                for form in range(total_forms):
+                    number_pattern = re.compile("(0/91)?[6-9][0-9]{9}")
+                    phone_number = self.data.get('authentication-spocdetails-content_type-object_id-' + str(form) + '-number')
+                    if phone_number and number_pattern.match(str(phone_number)):
+                        phone_no_flag = True
+                if phone_no_flag == False:
+                    raise forms.ValidationError("Atleast one mobile no is required for SPOC Details")
 
         if (not self.request.user.is_superuser and not self.request.user.groups.filter(name=constants['SUPER_QC_GROUP']).exists()):
             # and (not '_reopen' in self.data and not self.request.user.groups.filter(name__in=[constants['QC_GROUP_NAME'], constants['WELCOME_CALLING_TEAM']]).exists()):
