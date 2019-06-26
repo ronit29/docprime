87c0b05e33205520ca8dcc7f0c58e10519e37ee0
Arun Chaudhary <arunchaudhary@policybazaar.com>
2019-06-26 20:44:46 +0530

    fixes

--- ondoc/account/models.py
+++ ondoc/account/models.py
@@ -1551,9 +1551,53 @@ class MerchantPayout(TimeStampedModel):
     #         if p.utr_no:
     #             p.create_insurance_transaction()
 
+    def should_create_insurance_transaction(self):
+        from ondoc.insurance.models import InsuranceTransaction
+
+        premium_amount = None
+        transferred_amount = 0
+        user_insurance = None
+        all_payouts = None
+        pms = PayoutMapping.objects.filter(payout=self).first()
+        if pms:
+            user_insurance = pms.content_object
+        if user_insurance:
+            premium_amount = user_insurance.premium_amount
+            all_payouts = PayoutMapping.objects.filter(object_id=user_insurance.id, content_type_id=\
+                ContentType.objects.get_for_model(user_insurance).id).\
+                exclude(payout__paid_to_id=settings.DOCPRIME_NODAL2_MERCHANT).exclude(payout=self)
+
+            all_payouts = [x.payout for x in all_payouts]
+            transfers = InsuranceTransaction.objects.filter(user_insurance=user_insurance,\
+             transaction_type=InsuranceTransaction.CREDIT, reason=InsuranceTransaction.PREMIUM_PAYOUT)
+            transfers = list(transfers)
+
+            for transfer in transfers:
+                transferred_amount += transfer.amount
+            total_payouts = len(all_payouts)
+            counter = 0
+
+            for payout in all_payouts:
+                for transfer in transfers: 
+                    if not hasattr(payout,'_removed'):
+                        payout._removed = False
+                    if not hasattr(transfer,'_removed'):
+                        transfer._removed = False
+                   
+                    if not payout._removed and not transfer._removed and payout.payable_amount == transfer.amount:
+                        transfer._removed = True
+                        payout._removed = True
+
+            all_payouts = [x for x in all_payouts if not x._removed]
+            transfers = [x for x in transfers if not x._removed]
+            if len(transfers)==0 and (transferred_amount+self.payable_amount)<=premium_amount:
+                return True
+
+
+
     def create_insurance_transaction(self):
         from ondoc.insurance.models import UserInsurance, InsuranceTransaction
-        if not self.get_insurance_transaction():
+        if True or not self.get_insurance_transaction():
             user_insurance = self.get_user_insurance()
             # InsuranceTransaction.objects.create(user_insurance=user_insurance,
             #     # account = user_insurance.insurance_plan.insurer.float.first(),
