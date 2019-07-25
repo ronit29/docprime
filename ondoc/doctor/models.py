de303f9825f0ced80eeaaaa1d7aba43cadcee474
Sonam Sinha <sonamsinha@policybazaar.com>
2019-07-25 18:43:48 +0530

    fixes

--- ondoc/doctor/models.py
+++ ondoc/doctor/models.py
@@ -958,7 +958,7 @@ class Doctor(auth_model.TimeStampedModel, auth_model.QCModel, SearchKey, auth_mo
     def update_deal_price(self):        
         # will update only this doctor prices and will be called on save
         query = '''update doctor_clinic_timing set deal_price = 
-					case when (custom_deal_price is null or custom_deal_price = 0 )
+					case when (custom_deal_price > 0 )
 					then custom_deal_price else floor(				 
                     case when fees =0 then
 						case when mrp <300 then least((0.5*mrp)/0.75, mrp)
@@ -986,7 +986,7 @@ class Doctor(auth_model.TimeStampedModel, auth_model.QCModel, SearchKey, auth_mo
     def update_all_deal_price(cls):
         # will update all doctors prices
         query = '''update doctor_clinic_timing set deal_price = 
-					case when (custom_deal_price is null or custom_deal_price = 0 )
+					case when (custom_deal_price > 0 )
 					then custom_deal_price else floor(				 
                     case when fees =0 then
 						case when mrp <300 then least((0.5*mrp)/0.75, mrp)
