1c55f63aac377543c44cca0e2416d6fc6dea0250
Mayank <mayankgupta@docprime.com>
2019-07-29 22:33:11 +0530

    doctor data

--- ondoc/integrations/Integrators/__init__.py
+++ ondoc/integrations/Integrators/__init__.py
@@ -1,7 +1,9 @@
 from .thyrocare import Thyrocare
+from .sims import Sims
 
 integrator_mapping = {
-    'Thyrocare': Thyrocare
+    'Thyrocare': Thyrocare,
+    'Sims': Sims
 }
 
 __all__ = ["integrator_mapping"]
