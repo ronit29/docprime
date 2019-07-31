2fabe776813614c2893dc7555fcc94494e957455
Rohit Porwal <rohitporwal@docprime.com>
2019-07-31 16:17:31 +0530

    remove super qc to capture payment

--- ondoc/crm/admin/lab.py
+++ ondoc/crm/admin/lab.py
@@ -967,8 +967,7 @@ class LabAppointmentAdmin(nested_admin.NestedModelAdmin):
 
     def response_change(self, request, obj):
         if "_capture-payment" in request.POST:
-            if (request.user.is_superuser or request.user.groups.filter(
-                    name=constants['SUPER_QC_GROUP']).exists()):
+            if request.user.is_superuser:
                 txn_obj = obj.get_transaction()
                 if txn_obj and txn_obj.is_preauth():
                     notification_tasks.send_capture_payment_request.apply_async(
