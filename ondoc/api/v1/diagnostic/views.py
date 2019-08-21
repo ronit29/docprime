b3d309e1552152cc681c0e9ec83fa5e481b2ff52
Rohit Porwal <rohitporwal@docprime.com>
2019-08-21 16:01:45 +0530

    test_ids in pathology response

--- ondoc/api/v1/diagnostic/views.py
+++ ondoc/api/v1/diagnostic/views.py
@@ -2873,7 +2873,8 @@ class LabTimingListView(mixins.ListModelMixin,
                     integrator_obj = service.create_integrator_obj(class_name)
                     lab_timings = integrator_obj.get_appointment_slots(pincode, date, is_home_pickup=pathology_pickup)
 
-                pathology_resp = {"tests_name": ', '.join(list(map(lambda x: x.name, pathology_tests))),
+                pathology_resp = {"tests_id": list(map(lambda x: x.id, pathology_tests)),
+                                  "tests_name": ', '.join(list(map(lambda x: x.name, pathology_tests))),
                                   "timeslots": lab_timings.get('time_slots', []),
                                   "upcoming_slots": lab_timings.get('upcoming_slots', []),
                                   "is_thyrocare": lab_timings.get('is_thyrocare', False)}
