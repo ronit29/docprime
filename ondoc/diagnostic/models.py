dc38cba3c22e2e20e372302364c5c378131b9e16
Mayank <mayankgupta@docprime.com>
2019-08-28 21:32:59 +0530

    check

--- ondoc/diagnostic/models.py
+++ ondoc/diagnostic/models.py
@@ -1868,11 +1868,11 @@ class LabAppointment(TimeStampedModel, CouponsMixin, LabAppointmentInvoiceMixin,
                 logger.error(str(e))
 
         if push_to_matrix and self.booked_by_spo():
-            try:
-                push_appointment_to_spo.apply_async(({'type': 'LAB_APPOINTMENT', 'appointment_id': self.id, 'product_id': 5,
-                                                    'sub_product_id': 2},), countdown=5)
-            except Exception as e:
-                logger.error(str(e))
+            # try:
+            #     push_appointment_to_spo.apply_async(({'type': 'LAB_APPOINTMENT', 'appointment_id': self.id, 'product_id': 5,
+            #                                         'sub_product_id': 2},), countdown=5)
+            # except Exception as e:
+            #     logger.error(str(e))
 
             # try:
             #     push_appointment_to_spo_1.apply_async(({'type': 'LAB_APPOINTMENT', 'appointment_id': self.id, 'product_id': 5,
@@ -1880,11 +1880,11 @@ class LabAppointment(TimeStampedModel, CouponsMixin, LabAppointmentInvoiceMixin,
             # except Exception as e:
             #     logger.error(str(e))
 
-            # try:
-            #     push_appointment_to_spo_2.apply_async(({'type': 'LAB_APPOINTMENT', 'appointment_id': self.id, 'product_id': 5,
-            #                                         'sub_product_id': 2},), countdown=5)
-            # except Exception as e:
-            #     logger.error(str(e))
+            try:
+                push_appointment_to_spo_2.apply_async(({'type': 'LAB_APPOINTMENT', 'appointment_id': self.id, 'product_id': 5,
+                                                    'sub_product_id': 2},), countdown=5)
+            except Exception as e:
+                logger.error(str(e))
 
         is_thyrocare_enabled = False
         if not self.created_by_native():
