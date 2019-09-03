3c497853ae37f4db68126e10e684ac5b5b99e628
Akash Saini <akashs@docprime.com>
2019-09-03 12:47:03 +0530

    diable routing

--- ondoc/api/v1/plus/router.py
+++ ondoc/api/v1/plus/router.py
@@ -5,6 +5,6 @@ from .views import PlusListViewSet, PlusOrderViewSet
 urlpatterns = [
     path('list', PlusListViewSet.as_view({'get': 'list'}), name='plus-list'),
     path('lead/create', PlusOrderViewSet.as_view({'post': 'create_plus_lead'}), name='plus-lead-create'),
-    path('create', PlusOrderViewSet.as_view({'post': 'create_order'}), name='plus-subscription-create'),
+    # path('create', PlusOrderViewSet.as_view({'post': 'create_order'}), name='plus-subscription-create'),
 ]
 
