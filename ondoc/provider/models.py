6e0b6e6330ef2783a6cc6f35cd5bd1bec8ac8ede
Navneet Singh <navneetsingh@docprime.com>
2019-09-06 15:57:28 +0530

    migration

--- ondoc/provider/models.py
+++ ondoc/provider/models.py
@@ -234,7 +234,7 @@ class ProviderLabTestSampleDetails(auth_models.TimeStampedModel):
     lab_test = models.ForeignKey(diag_models.LabTest, on_delete=models.CASCADE, related_name="sample_details")
     sample = models.ForeignKey(ProviderLabTestSamples, on_delete=models.CASCADE, related_name="details")
     volume = models.PositiveIntegerField(null=True, blank=True)                             # in milli-litres
-    fasting_required = models.BooleanField(default=False)                                   # in milli-litres
+    fasting_required = models.BooleanField(default=False)
     report_tat = models.PositiveSmallIntegerField(null=True, blank=True)                    # in hours
     reference_value = models.PositiveIntegerField(null=True, blank=True)
     material_required = JSONField()
@@ -244,3 +244,21 @@ class ProviderLabTestSampleDetails(auth_models.TimeStampedModel):
 
     class Meta:
         db_table = "provider_lab_test_sample_details"
+
+
+# class ProviderLabSamplesCollectOrder(auth_models.TimeStampedModel):
+#
+#     user_profile = models.ForeignKey(auth_models.UserProfile, on_delete=models.CASCADE, related_name="profile_lab_samples_collect_order")
+#     patient_details = JSONField()
+#     hospital = models.ForeignKey(doc_models.Hospital, on_delete=models.CASCADE, related_name="hosp_lab_samples_collect_order")
+#     doctor = models.ForeignKey(doc_models.Doctor, on_delete=models.CASCADE, related_name="doc_lab_samples_collect_order")
+#     available_lab_tests = models.ManyToManyField(diag_models.AvailableLabTest, related_name="tests_lab_samples_collect_order")
+#     collection_datetime = models.DateTimeField()
+#     samples = models.ForeignKey(auth_models.UserProfile, on_delete=models.CASCADE, related_name="lab_samples_collect_order")
+#     lab_alerts = models.ManyToManyField(diag_models.LabAlerts)
+#
+#     def __str__(self):
+#         return str(self.user_profile.name) + '-' + str(self.hospital.name)
+#
+#     class Meta:
+#         db_table = "provider_lab_samples_collect_order"
