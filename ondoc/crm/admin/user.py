74376ee625c8394ef17d9d1bb8e353722593d2bf
Mayank <mayankgupta@docprime.com>
2019-10-14 18:38:19 +0530

    register

--- ondoc/crm/admin/user.py
+++ ondoc/crm/admin/user.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import Permission
 from django.contrib.gis import admin
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.admin import UserAdmin
@@ -54,6 +55,7 @@ class CustomUserChangeForm(UserChangeForm):
             raise forms.ValidationError("Employee Code must be filled in staff profile.")
         return super().clean()
 
+
 class CustomUserAdmin(UserAdmin,VersionAdmin):
     list_display = ('email',)
     list_filter = ('is_staff', 'is_superuser')
@@ -66,6 +68,7 @@ class CustomUserAdmin(UserAdmin,VersionAdmin):
     list_display = ('email','phone_number', 'is_active')
     list_select_related = ('staffprofile',)
     form = CustomUserChangeForm
+
     def save_model(self, request, obj, form, change):
         if not obj.email:
             obj.email = None
@@ -91,6 +94,7 @@ class CustomUserAdmin(UserAdmin,VersionAdmin):
         if ordering:
             qs = qs.order_by(*ordering)
         return qs
+
     def get_changeform_initial_data(self, request):
         return {'user_type': 1}
 
@@ -151,6 +155,11 @@ class UserNumberUpdateAdmin(admin.ModelAdmin):
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('name', 'email',)
-
     search_fields = ['email', 'name']
     autocomplete_fields = ['user']
+
+
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'codename')
+    search_fields = ['name', 'codename']
+    model = Permission
