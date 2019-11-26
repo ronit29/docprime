bf66442e7029156b3864d3ba17132b4d3c0e5c35
ronit <ronitd@docprime.com>
2019-11-26 13:28:18 +0530

    up

--- ondoc/api/v1/auth/serializers.py
+++ ondoc/api/v1/auth/serializers.py
@@ -390,17 +390,16 @@ class RefreshJSONWebTokenSerializer(serializers.Serializer):
                 uid = get_date[0]
                 date_generated = get_date[1]
             time_elapsed = v1_utils.get_time_delta_in_minutes(date_generated)
-            if time_elapsed >2:
+            if time_elapsed > 2:
                 raise serializers.ValidationError('Reset Key Expired')
             else:
                 user = User.objects.filter(id=uid).first()
+                blacllist_token = WhiteListedLoginTokens.objects.filter(token=token, user=user).delete()
                 token_object = JWTAuthentication.generate_token(user)
                 return {
                     'token': token_object['token'],
                     'payload': token_object['payload']
                 }
-
-
 
         payload = self.check_payload_custom(token=token)
         user = self.check_user_custom(payload=payload)
