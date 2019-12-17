6937291162cefdb9dac9d0ccf8a8777685b4a9bc
Ankit Singhal <ankit.s@policybazaar.com>
2019-12-17 18:45:47 +0530

    identical files

--- ondoc/account/models.py
+++ ondoc/account/models.py
@@ -1607,7 +1607,7 @@ class ConsumerAccount(TimeStampedModel):
                     if old_txn_obj.ref_txns:
                         ctx_objs.append(old_txn_obj.debit_from_ref_txn(self, 0, parent_ref, initiate_refund, balance_refund))
                     if old_txn_obj.balance and old_txn_obj.balance > 0:
-                        if old_txn_obj.action == ConsumerTransaction.PAYMENT:
+                        if old_txn_obj.source == ConsumerTransaction.WALLET_SOURCE:
                             ctx_objs.append(old_txn_obj.debit_from_balance(self))
                     old_txn_obj.save()
             else:
