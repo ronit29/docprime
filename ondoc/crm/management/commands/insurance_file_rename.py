e471e3c914e00b1bd46d6c5ea0fa1767e881d095
ronit <ronitd@docprime.com>
2020-01-06 16:25:33 +0530

    up

--- ondoc/crm/management/commands/insurance_file_rename.py
+++ ondoc/crm/management/commands/insurance_file_rename.py
@@ -2,7 +2,10 @@ from django.core.management.base import BaseCommand
 
 from ondoc.insurance.models import UserInsurance
 from django.db import transaction
-import uuid
+import uuid, requests
+from django.core.files.uploadedfile import TemporaryUploadedFile, InMemoryUploadedFile
+
+
 
 class Command(BaseCommand):
 
@@ -13,5 +16,20 @@ class Command(BaseCommand):
         queryset = UserInsurance.objects.all()
         for data in queryset:
             if data.coi:
-                file_name = data.coi.url
+                file_path = data.coi.url
+                request = requests.get(file_path, stream=True)
+                if request.status_code != requests.codes.ok:
+                    continue
+                file_name = file_path.split('/')[-1]
+                splited = file_name.split('.')
+                splited = splited[0]
+                file_name = str(splited) + '-' + str(uuid.uuid4().hex) + '.' + str(splited[-1])
+                temp_file = TemporaryUploadedFile(file_name, 'byte', 1000, 'utf-8')
+                for block in request.iter_content(1024 * 8):
+                    if not block:
+                        break
+                    temp_file.write(block)
+                data.coi = InMemoryUploadedFile(temp_file, None, file_name, 'application/pdf', temp_file.tell(), None)
+                data.save()
+
 
