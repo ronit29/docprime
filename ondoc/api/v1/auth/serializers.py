7e345ad5e2e2baeaeadfa50177598b2a9946ce39
ankushg <ankushg@docprime.com>
2020-02-13 10:54:24 +0530

    user profile changes

--- ondoc/api/v1/auth/serializers.py
+++ ondoc/api/v1/auth/serializers.py
@@ -223,8 +223,15 @@ class UserProfileSerializer(serializers.ModelSerializer):
         plus_membership = obj.get_plus_membership
         if not plus_membership:
             return resp
+        resp['is_member_allowed'] = False
+        resp['is_corporate_plan'] = False
+        plus_members_count = plus_membership.get_members.count()
         resp['expiry_date'] = plus_membership.expire_date.date()
         resp['total_members_allowed'] = plus_membership.plan.total_allowed_members
+        if plus_membership.plan.is_corporate:
+            resp['is_corporate_plan'] = True
+        if resp['total_members_allowed'] and resp['total_members_allowed'] > 0 and plus_members_count >=0 and (resp['total_members_allowed'] - plus_members_count > 0):
+            resp['is_member_allowed'] = True
         resp['purchase_date'] = plus_membership.purchase_date.date()
         primary_member = plus_membership.get_primary_member_profile()
         if primary_member:
