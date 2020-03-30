09b2089f503b34f9adb9e509eba18c483f59e775
Ankush Gupta <ankushg@docprime.com>
2020-03-30 06:57:50 +0000

    views.py edited online with Bitbucket - retail booking changes

--- ondoc/api/v1/diagnostic/views.py
+++ ondoc/api/v1/diagnostic/views.py
@@ -2600,7 +2600,7 @@ class LabAppointmentView(mixins.CreateModelMixin,
 
         profile = validated_data.get('profile')
         plus_plan = validated_data.get('plus_plan', None)
-        if not profile.is_insured_profile or not profile.get_plus_membership:
+        if not profile.is_insured_profile and not profile.get_plus_membership:
             return Response(status=status.HTTP_400_BAD_REQUEST, data={"error": "Appointment booking for new users has "
                                                                                "been temporarily suspended. "
                                                                                "Incovenience is deeply regretted."
